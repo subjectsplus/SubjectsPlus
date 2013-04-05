033ff6dfbaeb505c526811b29daa45a6978b8051
David Gonzalez <David@Davids-MacBook-Pro.local>
2013-04-05 09:17:44 -0400

    Add date range parameter to talkback service

--- api/classes/TalkbackWebService.php
+++ api/classes/TalkbackWebService.php
@@ -51,6 +51,16 @@ class TalkbackWebService extends sp_WebService implements WebService
 
 					$lobjFinalParams['tag'] = $lobjSplit;
 					break;
+                case 'startdate':
+                    $lstrValue = $lstrValue . ' 00:00:00';
+
+                    $lobjFinalParams['startdate'] = $lstrValue;
+                    break;
+                case 'enddate':
+                    $lstrValue = $lstrValue . ' 23:59:59';
+
+                    $lobjFinalParams['enddate'] = $lstrValue;
+                    break;
 				case 'max':
 					$lstrValue = scrubData($lstrValue, 'integer');
 
@@ -71,7 +81,7 @@ class TalkbackWebService extends sp_WebService implements WebService
 	 */
 	function generateQuery(Array $lobjParams)
 	{
-		$lstrQuery = "SELECT talkback_id, question, q_from, date_submitted, answer, CONCAT( fname, ' ', lname ) AS answered_by, display, tbtags
+		$lstrQuery = "SELECT talkback_id, question, q_from, date_submitted, answer, CONCAT( fname, ' ', lname ) AS answered_by, display, tbtags, cattags
           			FROM talkback, staff";
 
 		$lobjConditions = array();
@@ -92,6 +102,22 @@ class TalkbackWebService extends sp_WebService implements WebService
 
 					array_push($lobjConditions, $lstrCombine);
 					break;
+                case 'startdate':
+                    $lstrDateQuery = "date_submitted >= '$lobjValues'";
+
+                    if( isset( $lobjParams['enddate'] ) )
+                    {
+                        $lstrDateQuery .= " AND date_submitted <= '{$lobjParams['enddate']}'";
+                    }
+                    else
+                    {
+                        $lobjValues = str_replace( '00:00:00', "23:59:59", $lobjValues);
+
+                        $lstrDateQuery .= " AND date_submitted <= '$lobjValues'";
+                    }
+
+                    array_push($lobjConditions, $lstrDateQuery);
+                    break;
 			}
 		}
 
