aa51d2604892fb06e68deeb1a5e6d9b12e83f58e
agdarby <agdarby@gmail.com>
2014-03-03 14:38:19 -0500

    New changes to pluslet table, plus removed db prefix

--- sp3update.sql
+++ sp3update.sql
@@ -1,16 +1,23 @@
 /* Position vacant for staff table */
 
-ALTER TABLE `subjectsplus`.`staff` 
+ALTER TABLE `staff` 
 ADD COLUMN `position_vacant` INT(1) NULL DEFAULT 0 AFTER `lat_long`;
 
 
 /* Give the subject guide a parent */
 
-ALTER TABLE `subjectsplus`.`subject` 
+ALTER TABLE `subject` 
 ADD COLUMN `parent` BIGINT(20) NULL AFTER `extra`;
 
 
 /* Let the tab point to external url */
 
-ALTER TABLE `subjectsplus`.`tab` 
+ALTER TABLE `tab` 
 ADD COLUMN `external_url` VARCHAR(500) NULL AFTER `tab_index`;
+
+/* Changes to pluslet table to allow header tweaking */
+
+ALTER TABLE `pluslet` ADD `hide_titlebar` INT( 1 ) NOT NULL DEFAULT '0',
+ADD `collapse_body` INT( 1 ) NOT NULL DEFAULT '0',
+ADD `suppress_body` INT( 1 ) NOT NULL DEFAULT '0',
+ADD `titlebar_styling` INT NULL DEFAULT NULL 
