12c795c79abb45eda1ef22d543f10e76418aa969
agdarby <agdarby@gmail.com>
2014-03-04 09:01:52 -0500

    Changed field from int to varchar

--- sp3update.sql
+++ sp3update.sql
@@ -20,4 +20,4 @@ ADD COLUMN `external_url` VARCHAR(500) NULL AFTER `tab_index`;
 ALTER TABLE `pluslet` ADD `hide_titlebar` INT( 1 ) NOT NULL DEFAULT '0',
 ADD `collapse_body` INT( 1 ) NOT NULL DEFAULT '0',
 ADD `suppress_body` INT( 1 ) NOT NULL DEFAULT '0',
-ADD `titlebar_styling` INT NULL DEFAULT NULL 
+ADD `titlebar_styling` VARCHAR( 100 ) NULL 
