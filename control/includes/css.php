4ad26030c2ed645a99491f788abbdc768c5ac032
James Little <j.little@miami.edu>
2014-03-05 10:03:51 -0500

    Assetic fixes. No more DIRECTORY SEPARATOR

--- control/includes/css.php
+++ control/includes/css.php
@@ -10,7 +10,7 @@
     use Assetic\AssetWriter;
     use Assetic\Cache\FilesystemCache;
     use Assetic\Asset\AssetCache;
- 
+    
     // Define the asset and cache directories
     
     $assets = dirname(dirname (__DIR__)) . DIRECTORY_SEPARATOR . 'assets';
@@ -18,51 +18,51 @@
     
     // A new CSS file can be added by sticking it in the assets folder or in 3 steps you can add a file that needs to be called in a specific order.
     
-  
+    
     // Create references to specific files in the assest directory with the AssetManager
     
     $am = new AssetManager();
-
+    
     
     // Step 1.
     
     $am->set('pure', new AssetCache(
-                                        new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'shared' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'pure.css')
+                                    new FileAsset($assets . '/css/shared/pure.css')
+                                    ,new FilesystemCache($cache)
+                                    
+                                    ));
+    
+    $am->set('jqueryui', new AssetCache(
+                                        new FileAsset($assets . '/css/shared/jquery-ui.css')
                                         ,new FilesystemCache($cache)
                                         
                                         ));
     
-    $am->set('jqueryui', new AssetCache(
-                                     new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'shared' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'jquery-ui.css')
-                                     ,new FilesystemCache($cache)
-                                     
-                                     ));
-    
     $am->set('colorbox', new AssetCache(
-                                     new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'shared' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'colorbox.css')
-                                     ,new FilesystemCache($cache)
-                                     
-                                     ));
-
-      $am->set('admin_styles', new AssetCache(
-             new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'admin_styles.css')
-             ,new FilesystemCache($cache)
-                                     ));
+                                        new FileAsset($assets . '/css/shared/colorbox.css')
+                                        ,new FilesystemCache($cache)
+                                        
+                                        ));
+    
+    $am->set('admin_styles', new AssetCache(
+                                            new FileAsset($assets .'/css/admin/admin_styles.css')
+                                            ,new FilesystemCache($cache)
+                                            ));
     
     $am->set('guide', new AssetCache(
-             new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'admin'  . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'guide.css')
-             ,new FilesystemCache($cache)
+                                     new FileAsset($assets . '/css/admin/guide.css')
+                                     ,new FilesystemCache($cache)
                                      ));
-
-$am->set('override', new AssetCache(
-             new FileAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'override.css')
-             ,new FilesystemCache($cache)
-                                     ));    
+    
+    $am->set('override', new AssetCache(
+                                        new FileAsset($assets .'/css/admin/override.css')
+                                        ,new FilesystemCache($cache)
+                                        ));
     
     // Glob all the rest of the CSS files together
     
     
-   // $am->set('css', new AssetCache(new GlobAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR .  '*.css'), new FilesystemCache($cache)));
+    // $am->set('css', new AssetCache(new GlobAsset($assets . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR .  '*.css'), new FilesystemCache($cache)));
     
     
     
@@ -74,7 +74,7 @@ $am->set('override', new AssetCache(
     $colorbox = new AssetCollection(array (new AssetReference($am, 'colorbox')));
     $override = new AssetCollection(array (new AssetReference($am, 'override')));
     $admin_styles = new AssetCollection(array (new AssetReference($am, 'admin_styles')));
-
+    
     //$css_files = new AssetCollection(array (new AssetReference($am, 'css')));
     
     
@@ -86,7 +86,7 @@ $am->set('override', new AssetCache(
     $css = new AssetCollection(array ($pure, $colorbox, $admin_styles, $guide,  $jqueryui, $override) );
     
     // Tell the browser that this is CSS and that it should be cached
-
+    
     header('Cache-control: public');
     header('Content-Type: text/css');
     header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 21600));
