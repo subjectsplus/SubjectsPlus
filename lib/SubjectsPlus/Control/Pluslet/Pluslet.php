80a67bad81475c7c50438193e6629ee43ce9b5b5
James Little <j.little@miami.edu>
2014-03-17 16:19:55 -0400

    Gears work, but no save button pops up

--- lib/SubjectsPlus/Control/Pluslet/Pluslet.php
+++ lib/SubjectsPlus/Control/Pluslet/Pluslet.php
@@ -146,9 +146,9 @@ class Pluslet {
         	$this->_pluslet .= "<div class=\"titlebar_text\">$this->_title $this->_visible_id</div>
             <div class=\"titlebar_options\">$this->_icons</div>";
 
-            if ($hide_titlebar != 0) {
+           // if ($hide_titlebar != 0) {
                 $this->_pluslet .= self::boxSettings(); // add in our hidden div full of box config options
-            }
+           // }
 
 
             $this->_pluslet .= "</div>";
