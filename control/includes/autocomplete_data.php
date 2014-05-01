cb70f773440918d0233caca80f8462845e1d0bf5
James Little <j.little@miami.edu>
2014-05-01 15:22:26 -0400

    Autocomplete results based on the guide itself

--- control/includes/autocomplete_data.php
+++ control/includes/autocomplete_data.php
@@ -7,4 +7,5 @@ $auto_complete = new Autocomplete();
 
 $auto_complete->setParam($_GET["term"]);
 $auto_complete->setCollection($_GET["collection"]);
+$auto_complete->setSubjectId($_GET["subject_id"]);
 echo $auto_complete->search();
