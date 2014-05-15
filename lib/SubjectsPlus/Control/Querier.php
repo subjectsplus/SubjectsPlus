634d0e02282c1f5f76eed548502e6abcbb9ffb6f
David Gonzalez <david.gonzalez036@gmail.com>
2014-05-15 16:13:56 -0400

    Querier uses variables in config.php and create db object in header after checking if config file exists

--- lib/SubjectsPlus/Control/Querier.php
+++ lib/SubjectsPlus/Control/Querier.php
@@ -10,22 +10,22 @@ namespace SubjectsPlus\Control;
  *   @date April, 2014
  *   @todo
  */
-    
+
 // Fetch styles: http://www.php.net/manual/en/pdostatement.fetch.php
 // Example:
-    
+
 /*
-     
+
      $q = new Querier;
      $rows = $q->query('SELECT * FROM department');
      print_r($rows);
-     
+
      foreach ($rows as $value) {
-     
+
      echo $value['name'];
-     
+
      }
-     
+
 */
 
 use PDO;
@@ -36,19 +36,20 @@ class Querier  {
     private $_connection;
 
     function __construct() {
-        // This creates the connection by reading the credentials from a db.ini file
-        $config = parse_ini_file(dirname(dirname(dirname(__DIR__))) . "/control/includes/db.ini", true);
+        // This creates the connection by reading the config.php
 
-        $dsn = 'mysql:dbname=' . $config['database']['dbname'] . ';host=' . $config['database']['host'] . ';port=' . $config['database']['port'] . ';charset=' . $config['database']['charset'];
+    	global $hname;
+    	global $uname;
+    	global $pword;
+    	global $dbName_SPlus;
 
-        $username = $config['database']['username'];
-        $password = $config['database']['password'];
+        $dsn = 'mysql:dbname=' . $dbName_SPlus . ';host=' . $hname . ';charset=utf8';
 
         try {
-            $this->_connection = new PDO($dsn, $username, $password, array(PDO::ATTR_PERSISTENT => true));
+            $this->_connection = new PDO($dsn, $uname, $pword, array(PDO::ATTR_PERSISTENT => true));
         } catch (\PDOException $e) {
 
-            
+
             echo "<h1>There was a problem connecting to the database.</h1>";
             echo "<p>Have you set the database credentials in /control/includes/db.ini?</p>";
             echo "<p>This is the detailed error:</p>";
@@ -57,60 +58,60 @@ class Querier  {
     }
 
     public function query($sql, $fetch_style = NULL) {
-        
+
         // Default is numbered array
         if ($fetch_style === NULL) {
             $fetch_style = PDO::FETCH_BOTH;
         }
 
         $connection = $this->_connection;
-    
+
            $result = $connection->query($sql);
         if (!$result) {
-            
+
             echo "<p><h2>Woah! There was a problem with that query.</h2> Maybe this will help: ";
             print_r($connection->errorInfo());
             echo "</p>";
             echo $sql;
-            
+
             $rows = NULL;
         } else {
            $rows = $result->fetchAll($fetch_style);
         }
-     
+
 
         return $rows;
     }
-    
+
     public function exec($sql) {
         $connection = $this->_connection;
         $result = $connection->exec($sql);
         return $result;
-        
+
     }
-    
+
     public function fetch($sql) {
         $connection = $this->_connection;
         $result = $connection->query($sql);
         $fetch_row = $connection->fetchColumn();
         return $fetch_row;
-        
+
     }
- 
+
     public function num_rows($sql) {
         $connection = $this->_connection;
         $result = $connection->query($sql);
         return count($result);
     }
 
- 
+
     public function quote($string) {
         $connection = $this->_connection;
         $quoted_string = $connection->quote($string);
         return $quoted_string;
-        
+
     }
-    
+
     public function last_id() {
         $connection = $this->_connection;
         $last_id = $connection->lastInsertId();
