3687d9d01ad35069d2917cbed4c0a39ba9aa3b05
James Little <j.little@miami.edu>
2014-06-10 14:26:21 -0400

    libguides importer tabs

--- control/includes/libguides_importer.php
+++ control/includes/libguides_importer.php
@@ -11,21 +11,47 @@ $db = new Querier;
 
 $libguides_xml= new SimpleXMLElement(file_get_contents('libguides.xml','r'));
 
-$subject_values = zip($libguides_xml->xpath('//GUIDE/NAME'), $libguides_xml->xpath('//GUIDE/GUIDE_ID'), $libguides_xml->xpath('//GUIDE/LAST_UPDATE'), $libguides_xml->xpath('//GUIDE/DESCRIPTION'));
+$subject_values = zip($libguides_xml->xpath('//GUIDE/NAME'), 
+		      $libguides_xml->xpath('//GUIDE/GUIDE_ID'), 
+		      $libguides_xml->xpath('//GUIDE/LAST_UPDATE'), 
+		      $libguides_xml->xpath('//GUIDE/DESCRIPTION'), 
+		      $libguides_xml->xpath('//GUIDE/PAGES'));
 
-foreach($subject_values as $name) { 
 
-  if($db->exec("INSERT INTO subject (subject, subject_id, last_modified, description) VALUES ('$name[0]', '$name[1]', '$name[2]', '$name[3]')")) {
+foreach($subject_values as $subject) { 
 
-    echo "Inserted '$name[0]' \n";
+  if($db->exec("INSERT INTO subject (subject, subject_id, last_modified, description) VALUES ('$subject[0]', '$subject[1]', '$subject[2]', '$subject[3]')")) {
+
+    echo "Inserted '$subject[0]' \n";
 
   } else {
-    echo "Problem inserting '$name[0]'. This subject guide may already exist in the database. \n";
+
+    echo "Problem inserting '$subject[0]'. This subject guide may already exist in the database. \n";
+    echo "\tError: ";
+    echo  $db->errorInfo()[2] . "\n \n";
+  
+  }
+
+  $subject_page = $subject[4];
+
+  foreach ($subject_page->PAGE as $tab) {
+
+    if($db->exec("INSERT INTO tab (tab_id, subject_id, label) VALUES ('$tab->PAGE_ID', '$subject[1]', '$tab->NAME')")) {
+      
+      echo  "Inserted tab '$tab->NAME' \n";
+
+    } else {
+
+      echo "Problem inserting the tab, '$tab->NAME'. This tab may already exist in the database. \n";
+      
+      echo "\tError: ";
+      echo  $db->errorInfo()[2] . "\n \n";
+
+    }
+    
   }
 
 }
-
-
 
 
 
