16f014b627416c09b0d53983911051861b133bf0
David Gonzalez <david.gonzalez036@gmail.com>
2014-06-12 14:58:22 -0400

    no title pluslet pulsate fix

--- lib/SubjectsPlus/Control/Pluslet/Pluslet.php
+++ lib/SubjectsPlus/Control/Pluslet/Pluslet.php
@@ -134,7 +134,7 @@ class Pluslet {
 
         if (($hide_titlebar == 1 && $this->_visible_id == "" || $this->_title == "" && $this->_visible_id == "")) {
             $this->_pluslet .= "
-            <div class=\"pluslet_simple no_overflow\"><a name=\"box-" . $this->_pluslet_id . "\"></a>" . htmlspecialchars_decode($this->_body);
+            <div id=\"$this->_pluslet_id_field\" class=\"pluslet_simple no_overflow $this->_pluslet_id_field\"><a name=\"box-" . $this->_pluslet_id . "\"></a>" . htmlspecialchars_decode($this->_body);
             // this div closed outside of if/else
         } else {
 
