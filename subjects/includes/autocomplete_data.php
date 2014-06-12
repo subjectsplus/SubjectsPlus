fe90134bc0cf8fda2367fd5d390d4e9cd3732513
James Little <j.little@miami.edu>
2014-06-12 09:44:49 -0400

    isset everything

--- subjects/includes/autocomplete_data.php
+++ subjects/includes/autocomplete_data.php
@@ -12,8 +12,18 @@ include("../../control/includes/autoloader.php");
 
 $auto_complete = new Autocomplete();
 $auto_complete->setSearchPage("public");
-$auto_complete->setParam($_GET["term"]);
-$auto_complete->setCollection($_GET["collection"]);
-$auto_complete->setSubjectId($_GET["subject_id"]);
+
+if (isset($_GET["term"])) {
+  $auto_complete->setParam($_GET["term"]);
+}
+
+if  (isset($_GET["collection"])) {
+  $auto_complete->setCollection($_GET["collection"]);
+}
+if (isset($_GET["subject_id"])) {
+  $auto_complete->setSubjectId($_GET["subject_id"]);
+}
+
+
 echo $auto_complete->search();
 
