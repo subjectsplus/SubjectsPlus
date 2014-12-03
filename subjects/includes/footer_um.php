6292c40bd6b3f3361dda600e87b5f3cdbfdb179b
pvillanueva01 <pvillanueva@libwet238.local>
2014-12-03 11:42:40 -0500

    Custom CHC header/footer for guides (um theme), adjustment to header padding

--- subjects/includes/footer_um.php
+++ subjects/includes/footer_um.php
@@ -16,12 +16,12 @@ switch ($our_site) {
         (305) 284-4900<br />
         Email:  <a href=\"mailto:chc@miami.edu\">chc@miami.edu</a>";
     $social_icons = "<p id=\"social_icons_small\">
-  <a href=\"" . PATH_TO_CHILD . "/feed/\"><img src=\"" . THEME_BASE_DIR . "/images/rss_26.png\"  alt=\"Visit the CHC Blog\" title=\"Visit the CHC Blog\" /></a>
-  <a href=\"" . PATH_TO_CHILD . "/community/subscribe/\"><img src=\"" . THEME_BASE_DIR . "/images/mailing_list.png\"  alt=\"Join our Mailing List\" title=\"Join our Mailing List\" /></a>
-      <a href=\"http://www.facebook.com/umchc\"><img src=\"" . THEME_BASE_DIR . "/images/facebook.png\"  alt=\"Find Us on Facebook\" title=\"Find Us on Facebook\" /></a>
-    <a href=\"https://twitter.com/umchc\" border=\"0\"><img src=\"" . THEME_BASE_DIR . "/images/twitter.png\"  alt=\"Twitter\" title=\"Twitter\" /></a>
-    <a href=\"http://vimeo.com/umchc\"><img src=\"" . THEME_BASE_DIR . "/images/vimeo.png\"  alt=\"Find Us on Vimeo\" title=\"Find Us on Vimeo\" /></a>
-      <a href=\"http://www.flickr.com/photos/umdigital/collections/72157623554504931/\" border=\"0\"><img src=\"" . THEME_BASE_DIR . "/images/flickr.png\"  alt=\"Find us on Flickr\" title=\"Find Us on Flickr\" /></a>
+  <a href=\"" . PATH_TO_CHILD . "/feed/\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/rss_26.png\"  alt=\"Visit the CHC Blog\" title=\"Visit the CHC Blog\" /></a>
+  <a href=\"" . PATH_TO_CHILD . "/community/subscribe/\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/mailing_list.png\"  alt=\"Join our Mailing List\" title=\"Join our Mailing List\" /></a>
+      <a href=\"http://www.facebook.com/umchc\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/facebook.png\"  alt=\"Find Us on Facebook\" title=\"Find Us on Facebook\" /></a>
+    <a href=\"https://twitter.com/umchc\" border=\"0\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/twitter.png\"  alt=\"Twitter\" title=\"Twitter\" /></a>
+    <a href=\"http://vimeo.com/umchc\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/vimeo.png\"  alt=\"Find Us on Vimeo\" title=\"Find Us on Vimeo\" /></a>
+      <a href=\"http://www.flickr.com/photos/umdigital/collections/72157623554504931/\" border=\"0\"><img src=\"http://library.miami.edu/wp-content/themes/umiami/images/flickr.png\"  alt=\"Find us on Flickr\" title=\"Find Us on Flickr\" /></a>
     </p>";
   break;
   default:
