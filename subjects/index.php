6b15c4047c3632cdd2fcf7e9cf98b5dc715da831
Andrew Darby <agdarby@gmail.com>
2014-12-16 16:08:01 -0500

    the code wasn't right to use non-themed content

--- subjects/index.php
+++ subjects/index.php
@@ -14,7 +14,7 @@ include("../control/includes/functions.php");
 include("../control/includes/autoloader.php");
 
 // If you have a theme set, but DON'T want to use it for this page, comment out the next line
-if (isset($subjects_theme)) { include("themes/$subjects_theme/index.php"); exit;}
+if (isset($subjects_theme)  && $subjects_theme != "") { include("themes/$subjects_theme/index.php"); exit;}
    
 $use_jquery = array("ui");
 
