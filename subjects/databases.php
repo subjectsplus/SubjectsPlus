13a527987e75e2f30afafdc412c419c7a6894d0a
Andrew Darby <agdarby@gmail.com>
2014-12-16 16:08:01 -0500

    the code wasn't right to use non-themed content

--- subjects/databases.php
+++ subjects/databases.php
@@ -16,7 +16,7 @@ include("../control/includes/functions.php");
 include("../control/includes/autoloader.php");
 
 // If you have a theme set, but DON'T want to use it for this page, comment out the next line
-if (isset($subjects_theme)) { include("themes/$subjects_theme/databases.php"); exit;}
+if (isset($subjects_theme)  && $subjects_theme != "") { include("themes/$subjects_theme/databases.php"); exit;}
 
 $db = new Querier;
     
