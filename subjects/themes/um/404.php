7ab14a1ccb1f5020fe2a4c41367f681e0a633f1c
pvillanueva01 <pvillanueva@libwet238.local>
2015-01-16 16:59:24 -0500

    more fixes to the responsive table views for staff list

---
+++ subjects/themes/um/404.php
@@ -0,0 +1,40 @@
+<?php
+
+$page_title = "Page not found";
+$description = "404 Page not found";
+$keywords = "404, missing page";
+
+$use_jquery = array("ui", "ui_styles");
+
+
+include("includes/header_um.php");
+
+?>
+
+
+<div class="panel-container panel-adj">
+<div class="pure-g">
+      <div class="pure-u-1">
+          <div class="breather">
+          <p>test 404 page</p>
+          </div>
+      </div>
+
+      
+
+</div><!--end pure-g-->
+</div> <!--end panel-container-->
+
+
+<!--Data Table-->
+
+
+<?php
+
+////////////
+// Footer
+///////////
+
+include("includes/footer_um.php");
+
+?>
