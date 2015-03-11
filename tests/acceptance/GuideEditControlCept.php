c7766624e77cd2bfaa3176034d7efa5cb3517ccb
Jamie Little <j.little@miami.edu>
2015-03-11 10:27:03 -0400

    GuideEdit test now tests tabs

--- tests/acceptance/GuideEditControlCept.php
+++ tests/acceptance/GuideEditControlCept.php
@@ -5,11 +5,30 @@ TestCommons::logMeIn($I);
 $I->amOnPage('control/guides/');
 $I->see('Edit Your Guides');
 $I->click('//*[@id="maincontent"]/div/div[1]/div/div[2]/div[1]/a[4]');
-$I->see('Drop Here');
+
+
+// Test dropping a new box
+
+
 $I->moveMouseOver('#newbox');
 $I->dragAndDrop('//*[@id="pluslet-id-Basic"]','//*[@id="dropspot-left-1"]');
+
 $I->see('SAVE CHANGES');
 $I->waitForElement('.cke_chrome');
 $I->fillField(['name'=>'new_pluslet_title'], 'Testing');
-$I->click('Save Changes');
+
+
+// Test new tab 
+
+$I->waitForElement('#add_tab');
+$I->click('#add_tab');
+$I->fillField('#tab_title',"Testing");
+$I->click('Add');
+$I->see('Drop Here');
+
+
+// Test saving
+$I->click('SAVE CHANGES');
+$I->click('SAVE CHANGES');
+$I->waitForElement('#response');
 $I->see('Thy');
