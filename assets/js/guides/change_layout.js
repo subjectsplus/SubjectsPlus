2b2673fca07a5e38b00faf29df55726eb1293d0c
James Little <j.little@miami.edu>
2015-07-10 14:19:40 -0400

    Adding slider code

---
+++ assets/js/guides/change_layout.js
@@ -0,0 +1,8 @@
+function changeLayout(first_column, second_column) {
+
+	$("[id^=slider]").first().slider('values',0, first_column);
+	$("[id^=slider]").first().slider('values',1,second_column );
+
+	
+
+}
