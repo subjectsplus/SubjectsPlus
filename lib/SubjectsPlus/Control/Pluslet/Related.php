311b1adef119fe6f22761ade1ae85bad2b1c6006
James Little <j.little@miami.edu>
2015-07-15 14:37:55 -0400

    Fix for related pluslet

--- lib/SubjectsPlus/Control/Pluslet/Related.php
+++ lib/SubjectsPlus/Control/Pluslet/Related.php
@@ -7,6 +7,7 @@ class Pluslet_Related extends Pluslet {
 	public function __construct($pluslet_id, $flag = "", $subject_id, $isclone = 0) {
 		parent::__construct ( $pluslet_id, $flag, $subject_id, $isclone );
 		
+		$this->_subject_id = $subject_id;
 		$this->_type = "Related";
 		
 		$this->db = new Querier ();
@@ -16,13 +17,17 @@ class Pluslet_Related extends Pluslet {
 		$this->_body = "<div>" . _("This pluslet automatically links to any child guides that you have assigned to this guide.") . "</div>";
 	}
 	public function outputRelatedGuides() {
+		
+		
 		$output = "";
 		
 		$output .= "<ul>";
 		
-		$children = $this->db->query ( 'SELECT subject_child  FROM subject NATURAL JOIN subject_subject WHERE subject_id = 653877' );
+		
+		$children = $this->db->query ( 'SELECT * FROM subject INNER JOIN subject_subject ON subject.subject_id = subject_subject.subject_child WHERE subject_parent = ' . $this->_subject_id );
 		
 		foreach ( $children as $child ) {
+			echo "SELECT * FROM subject WHERE subject_id = {$child['subject_child']} ";
 			$child_info = $this->db->query ( "SELECT * FROM subject WHERE subject_id = {$child['subject_child']} " );
 			
 			$output .= "<li><a href=\"../../subjects/guide.php?subject={$child_info[0]['shortform']}\">{$child_info[0]['subject']}</a></li>";
