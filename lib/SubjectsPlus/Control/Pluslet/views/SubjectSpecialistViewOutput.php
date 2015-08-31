f6bb2deae33adda168d244b3f0b4f44101c2305a
cbrownroberts <charlesbrownroberts@miami.edu>
2015-08-31 11:21:34 -0400

    added conditionals to social media icons on viewOutput

--- lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistViewOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistViewOutput.php
@@ -1,7 +1,8 @@
 <div id="subjectSpecialistPluslet">
 
+
     <?php if($this->staffPhoto != "off") {
-        echo  "<span id='staffPhoto'>{$this->staffPhoto}</span>";
+        echo  "<img id='staffPhoto' src='{$this->staffPhoto}' />";
     } ?>
 
 
@@ -22,11 +23,25 @@
         } ?>
 
     </ul>
+
     <ul>
-        <li><a href='http://facebook.com/<?php echo $this->staffFacebook; ?>'><i class='fa fa-facebook-square fa-3x'></i></a></li>
-        <li><a href='http://twitter.com/<?php echo $this->staffTwitter; ?>'><i class='fa fa-twitter-square fa-3x'></i></a></li>
-        <li><a href='http://pinterest.com/<?php echo $this->staffPinterest; ?>'><i class='fa fa-pinterest-square fa-3x'></i></a></li>
+        <?php if($this->staffFacebook) {
+
+            echo "<li><a href='http://facebook.com/{$this->staffFacebook}'><i class='fa fa-facebook-square fa-3x'></i></a></li>";
+        } ?>
+
+        <?php if($this->staffTwitter) {
+
+            echo "<li><a href='http://twitter.com/{$this->staffTwitter}'><i class='fa fa-twitter-square fa-3x'></i></a></li>";
+        } ?>
+
+        <?php if($this->staffPinterest) {
+
+            echo "<li><a href='http://pinterest.com/{$this->staffPinterest}'><i class='fa fa-pinterest-square fa-3x'></i></a></li>";
+        } ?>
+
     </ul>
+
 
 
 </div>
