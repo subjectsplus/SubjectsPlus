5b00eb9a8ba2c19c5a9d049e9b30459178047674
cbrownroberts <charlesbrownroberts@miami.edu>
2015-08-31 14:43:16 -0400

    Added message to get_user_favorite_boxes to indicate no boxes have been selected as a favorite.

--- assets/js/guides/get_user_favorite_boxes.js
+++ assets/js/guides/get_user_favorite_boxes.js
@@ -13,6 +13,13 @@ function get_user_favorite_boxes(staff_id) {
         data: {staff_id: staff_id},
         success: function(data) {
 
+            if(!data.length) {
+                //no results
+                $(".fav-boxes-list").append( "<li>No boxes have been marked as a favorite. To do so, click the gears button on the box you wish to mark as a Favorite and activate the Favorite toggle switch.</li>");
+
+
+            }
+
             $.each(data, function(idx, obj) {
                 $(".fav-boxes-list").append( "<li data-pluslet-id='" + obj.id +"' class='clone-favorite fav-box-item' title='" + obj.title + "'>" + obj.title + "</li>");
 
