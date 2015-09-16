4af4a4b739bd8f00e838f2a6781d818463e31561
cbrownroberts <charlesbrownroberts@miami.edu>
2015-09-16 10:07:50 -0400

    class and helper for grabbing user guides

---
+++ control/guides/helpers/user_guides.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbrownroberts
+ * Date: 9/15/15
+ * Time: 5:15 PM
+ */
---
+++ lib/SubjectsPlus/Control/Guide/UserGuide.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbrownroberts
+ * Date: 9/15/15
+ * Time: 5:05 PM
+ */
+
+namespace SubjectsPlus\Control\Guide;
+
+
+class UserGuide
+{
+
+}
