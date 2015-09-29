d91d018b7a2833ecfb0b37e9b72cb7d843541e77
cbrownroberts <charlesbrownroberts@miami.edu>
2015-09-29 13:07:41 -0400

    added pulsate box by tax on TOC link click for end user view

--- subjects/guide.php
+++ subjects/guide.php
@@ -342,24 +342,44 @@ console.log(tabs);
 
        $('.print-img-no-tabs').click(function(){ window.print(); });   
 
-	jQuery(document).ready(function() {
-	
-	   jQuery('a[href*="#tabs-"]').on('click', function(event, ui) {
-	     	event.preventDefault();
-		
-			var tab_id = event.target.hash.split('-')[1];
-	     	var box_id = event.target.hash.split('-')[2];
-	     	var selected_box = ".pluslet-" + box_id;
- if ($('#tabs-1').text()) {
-	     	$('#tabs').tabs('select', tab_id);
-}
-	     	jQuery(selected_box).effect("pulsate", {
-	     		times:1
-	     	}, 2000);
-	     	window.location.hash = 'box-' + box_id;
-         });
-		 });
-		 
+    jQuery(document).ready(function() {
+
+       jQuery('a[href*="#tabs-"]').on('click', function(event, ui) {
+           event.preventDefault();
+
+           var tab_id = event.target.hash.split('-')[1];
+           var box_id = event.target.hash.split('-')[2];
+           var selected_box = ".pluslet-" + box_id;
+           if ($('#tabs-1').text()) {
+               $('#tabs').tabs('select', tab_id);
+           }
+           jQuery(selected_box).effect("pulsate", {
+               times:1
+           }, 2000);
+           window.location.hash = 'box-' + box_id;
+       });
+
+
+       $('.table-of-contents').on('click', function(e){
+
+           e.preventDefault();
+           var tab_index = $(this).data("tab_index");
+           var pluslet_id = $(this).data("pluslet_id");
+           var box_id = $(this).attr("id");
+
+           $('#tabs').tabs('select', tab_index);
+
+           jQuery(box_id).effect("pulsate", {
+               times:2
+           }, 2000);
+           window.location.hash = 'box-' + box_id;
+
+       });
+
+
+
+    });
+
 		 
 	   
 	   
