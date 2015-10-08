d95a619475495ae32770b6255c2b3a3300638f5b
cbrownroberts <charlesbrownroberts@miami.edu>
2015-10-08 15:54:46 -0400

    added current_tab index to changeLayout function so that it works per tab index

--- assets/js/guides/change_layout.js
+++ assets/js/guides/change_layout.js
@@ -1,7 +1,8 @@
 function changeLayout(first_column, second_column) {
 
-	$("[id^=slider]").first().slider('values',0, first_column);
-	$("[id^=slider]").first().slider('values',1,second_column );
+	var current_tab = $('#tabs').tabs('option', 'selected');
+	var slider_section_id = $('#tabs-' + parseInt(current_tab)).children().attr('id');
 
-
+	$("#slider_" + slider_section_id).slider('values',0, first_column);
+	$("#slider_" + slider_section_id).slider('values',1, second_column );
 }
