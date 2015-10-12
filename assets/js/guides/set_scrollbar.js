8415a80b894d8cfeaf5b7dfca94bb030e4fa43c2
pvillanueva01 <pvillanueva@libwet238.local>
2015-10-12 13:31:37 -0400

    Scrollbar styles - to test in Windows IE, Firefox and Chrome

---
+++ assets/js/guides/set_scrollbar.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$('.box_options_container, .fav-boxes-content').enscroll({
+	    verticalTrackClass: 'track',
+	    verticalHandleClass: 'handle',
+	    minScrollbarLength: 28
+	});
+
+	    
+}); // End document.ready
+
+	
+
+
+
