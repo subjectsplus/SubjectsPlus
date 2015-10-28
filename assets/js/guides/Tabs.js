33d85ece7dfaa75839fb7f5064c5b1648fc74b3f
James Little <j.little@miami.edu>
2015-10-28 08:12:38 -0400

    Putting tabs functionality into its own object

---
+++ assets/js/guides/Tabs.js
@@ -0,0 +1,289 @@
+function getTabs() { 
+   var Tabs = {
+        settings: {
+            tabTitle : $("#tab_title"),
+            tabContent : $("#tab_content"),
+            tabCounter : $('#tabs').data().tabCount,
+            tabs: $("#tabs").tabs(),
+            
+            dialog: $("#dialog").dialog,
+            externalLink: 'input[name="tab_external_link"]',
+            dataExternalLink: 'li[data-external-link]',
+            saveButton: $("#save_guide"),
+            tabExternalUrl: 'input[name="tab_external_url"]'
+        },
+        strings: {
+            tabTemplate: "<li class=\"dropspotty\"><a href='#{href}'>#{label}</a><span class='alter_tab' role='presentation'><i class=\"fa fa-cog\"></i></span></li>"
+        },
+        bindUiActions: function () {
+        },
+        init: function () {
+            Tabs.setupTabs();
+        },
+        setupTabs: function () {
+
+            var myDialog = $("#dialog").dialog({
+                autoOpen: false,
+                modal: true,
+                buttons: {
+                    Add: function () {
+                        addTab();
+                        $(this).dialog("close");
+                    },
+                    Cancel: function () {
+                        $(this).dialog("close");
+                    }
+                },
+                open: function () {
+                    $(this).find(Tabs.settings.externalLink).hide();
+                    $(this).find(Tabs.settings.externalLink).prev().hide();
+                    if (Tabs.settings.tabCounter > 0) {
+                        $(this).find(Tabs.settings.externalLink).show();
+                        $(this).find(Tabs.settings.externalLink).prev().show();
+                    }
+                },
+                close: function () {
+                    form[0].reset();
+                }
+            });
+
+            //setup dialog to edit tab
+            var editTabDialog = $("#dialog_edit").dialog({
+                autoOpen: false,
+                modal: true,
+                width: "auto",
+                height: "auto",
+                buttons: {
+                    "Save": function () {
+                        var id = window.lastClickedTab.replace("#tabs-", "");
+
+                        $('a[href="#tabs-' + id + '"]').text($('input[name="rename_tab_title"]').val());
+                        $('a[href="#tabs-' + id + '"]').parent('li').attr('data-visibility', $('select[name="visibility"]').val());
+
+                        if ($('a[href="#tabs-' + id + '"]').parent('li').attr('data-external-link') != '') {
+                            $('a[href="#tabs-' + id + '"]').each(function () {
+                                var elementData = $._data(this),
+                                events = elementData.events;
+
+                                var onClickHandlers = events['click'];
+
+                                // Only one handler. Nothing to change.
+                                if (onClickHandlers.length == 1) {
+                                    return;
+                                }
+
+                                onClickHandlers.splice(0, 1);
+                            });
+                        }
+
+                        $('a[href="#tabs-' + id + '"]').parent('li').attr('data-external-link', $(Tabs.settings.tabExternalUrl).val());
+
+                        if ($(Tabs.settings.tabExternalUrl).val() != '') {
+                            $('a[href="#tabs-' + id + '"]').on('click', function (evt) {
+                                window.open($(this).parent('li').attr('data-external-link'), '_blank');
+                                evt.stopImmediatePropagation();
+                            });
+
+                            $('a[href="#tabs-' + id + '"]').each(function () {
+                                var elementData = $._data(this),
+                                events = elementData.events;
+
+                                var onClickHandlers = events['click'];
+
+                                // Only one handler. Nothing to change.
+                                if (onClickHandlers.length == 1) {
+                                    return;
+                                }
+
+                                onClickHandlers.splice(0, 0, onClickHandlers.pop());
+                            });
+                        }
+
+                        //add/remove class based on tab visibility
+                        if ($('select[name="visibility"]').val() == 1) {
+                            $('a[href="#tabs-' + id + '"]').parent('li').removeClass('hidden_tab');
+                        } else {
+                            $('a[href="#tabs-' + id + '"]').parent('li').addClass('hidden_tab');
+                        }
+
+                        $(this).dialog("close");
+                        $("#response").hide();
+                        $('#save_guide').fadeIn();
+                        //$('#save_template').fadeIn();
+                    },
+                    "Delete": function () {
+                        var id = window.lastClickedTab.replace("#tabs-", "");
+
+                        $('a[href="#tabs-' + id + '"]').parent().remove();
+                        $('div#tabs-' + id).remove();
+                        Tabs.settings.tabs("destroy");
+                        Tabs.settings.tabs.tabs();
+                        Tabs.settings.tabCounter--;
+                        $(this).dialog("close");
+                        $("#response").hide();
+                        $('#save_guide').fadeIn();
+                        //$('#save_template').fadeIn();
+                    },
+                    Cancel: function () {
+                        $(this).dialog("close");
+                    }
+                },
+                open: function (event, ui) {
+                    var id = window.lastClickedTab.replace("#tabs-", "");
+                    $(this).find('input[name="rename_tab_title"]').val($('a[href="#tabs-' + id + '"]').text());
+                    $(this).find('select[name="visibility"]').val($('a[href="#tabs-' + id + '"]').parent('li').attr('data-visibility'));
+
+                    //external url add text input unless first tab
+                    $(this).find(Tabs.settings.tabExternalUrl).val('');
+                    $(this).find(Tabs.settings.tabExternalUrl).hide();
+                    $(this).find(Tabs.settings.tabExternalUrl).prev().hide();
+                    $(this).find(Tabs.settings.tabExternalUrl).val($('a[href="#tabs-' + id + '"]').parent('li').attr('data-external-link'));
+                    if (id != '0') {
+                        $(this).find(Tabs.settings.tabExternalUrl).show();
+                        $(this).find(Tabs.settings.tabExternalUrl).prev().show();
+                    }
+                },
+                close: function () {
+                    form[0].reset();
+                }
+            });
+
+            //add click event for external url tabs
+            $(Tabs.settings.dataExternalLink).each(function () {
+                if ($(this).attr('data-external-link') != "") {
+                    $(this).children('a[href^="#tabs-"]').on('click', function (evt) {
+                        window.open($(this).parent('li').attr('data-external-link'), '_blank');
+                        evt.stopImmediatePropagation();
+                    });
+
+                    $(this).children('a[href^="#tabs-"]').each(function () {
+                        var elementData = $._data(this),
+                        events = elementData.events;
+
+                        var onClickHandlers = events['click'];
+
+                        // Only one handler. Nothing to change.
+                        if (onClickHandlers.length == 1) {
+                            return;
+                        }
+
+                        onClickHandlers.splice(0, 0, onClickHandlers.pop());
+                    });
+                }
+            });
+
+            // edit icon: removing or renaming tab on click
+            Tabs.settings.tabs.delegate("span.alter_tab", "click", function (lobjClicked) {
+                var List = $(this).parent().children("a");
+                var Tab = List[0];
+                window.lastClickedTab = $(Tab).attr("href");
+                editTabDialog.dialog("open");
+            });
+
+            // addTab button: just opens the dialog
+            $("#add_tab").button().click(function () {
+                myDialog.dialog("open");
+            });
+
+
+            // addTab form: calls addTab function on submit and closes the dialog
+            var form = myDialog.find("form").submit(function (event) {
+                addTab();
+                myDialog.dialog("close");
+                event.preventDefault();
+            });
+
+            // actual addTab function: adds new tab using the input from the form above
+            function addTab() {
+             var label = Tabs.settings.tabTitle.val() || "Tab " + Tabs.settings.tabCounter,
+                 external_link = $('input#tab_external_link').val(),
+                 id = "tabs-" + Tabs.settings.tabCounter,
+                 li = $(Tabs.strings.tabTemplate.replace(/#\{href\}/g, "#" + id).replace(/#\{label\}/g, label)),
+                tabContentHtml = Tabs.settings.tabContent.val() || "Tab " + Tabs.settings.tabCounter + " content.";
+                $(li).attr('data-external-link', external_link);
+                $(li).attr('data-visibility', 1);
+                Tabs.settings.tabs.find(".ui-tabs-nav").append(li);
+
+
+                //make tabs sortable
+                $(function () {
+                    $(tabs).find(".ui-tabs-nav").sortable({
+                        axis: "x",
+                        stop: function (event, ui) {
+                            if ($(ui.item).attr("id") == 'add_tab' || $(ui.item).parent().children(':first').attr("id") != 'add_tab' || $(ui.item).attr('data-external-link') != '')
+                                $(tabs).find(".ui-tabs-nav").sortable("cancel");
+                            else {
+                                // $(tabs).tabs( "refresh" );
+                                $(tabs).tabs("destroy");
+                                $(tabs).tabs();
+                                $(tabs).tabs('select', 0);
+                                $("#response").hide();
+                                $("#save_guide").fadeIn();
+                                //$("#save_template").fadeIn();
+                            }
+                        }
+                    });
+                });
+
+
+              $.ajax
+                ({
+                    url: "helpers/section_data.php",
+                    type: "POST",
+                    data: { action: 'create' },
+                    dataType: "html",
+                    success: function (html) {
+                        Tabs.settings.tabDestroy;
+
+                        Tabs.settings.tabs.append("<div id='" + id + "' class=\"sptab\">" + html
+                                     + "</div>");
+
+                        $("#response").hide();
+                        Tabs.settings.saveButton.fadeIn();
+                       
+
+                        Tabs.settings.tabs();
+
+                        if (external_link == '') {
+                            Tabs.settings.tabs('select', Tabs.settings.tabCounter);
+                        } else {
+                            Tabs.settings.tabs('select', 0);
+                        }
+
+                        if ($(li).attr('data-external-link') != '') {
+                            $(li).children('a[href^="#tabs-"]').on('click', function (evt) {
+                                window.open($(this).parent('li').attr('data-external-link'), '_blank');
+                                evt.stopImmediatePropagation();
+                            });
+                        }
+
+                        $(li).children('a[href^="#tabs-"]').each(function () {
+                            var elementData = $._data(this),
+                            events = elementData.events;
+
+                            var onClickHandlers = events['click'];
+
+                            // Only one handler. Nothing to change.
+                            if (onClickHandlers.length == 1) {
+                                return;
+                            }
+
+                            onClickHandlers.splice(0, 0, onClickHandlers.pop());
+                        });
+
+                        Tabs.settings.tabCounter++;
+                    }
+                });
+
+
+
+                //override submit for form in edit tab dialog to click rename button
+                $("#dialog_edit").find("form").submit(function (event) {
+                    $(this).parent().parent().find('span:contains("Rename")').click();
+                    event.preventDefault();
+                });
+            }
+        }
+    }
+    return Tabs;
+}
--- assets/js/guides/guide.js
+++ assets/js/guides/guide.js
@@ -1,103 +1,92 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var guide = getGuideBase();
+    guide.init();
+    var tabs = getTabs();
+    tabs.init();
+    var rs = getResourceList();
+    rs.init();
+    var fdbx = getFindBoxSearch();
+    fdbx.init();
 
-		
-		
-		
-		var clone_pluslet_id;
-		
-	    makeDropable(".dropspotty");
-	    console.log('make droppable');
-	    makeDropable(".cke");
-	    makeSortable(".sort-column");
-	    makeSortable(".sptab", 'sections');
-	    makeDraggable(".draggable");
+    var clone_pluslet_id;
 
-	    setupSaveButton('#save_guide');
+    makeDropable(".dropspotty");
+    console.log('make droppable');
+    makeDropable(".cke");
+    makeSortable(".sort-column");
+    makeSortable(".sptab", 'sections');
+    makeDraggable(".draggable");
 
-	    makeEditable('a[id*=edit]');
-	    makeDeleteable('a[id*=delete]');
-	    makeDeleteable('.section_remove', 'sections');
+    setupSaveButton('#save_guide');
 
-	    setupAllColorboxes();
-	    setupMiscEvents();
-	    setupMiscClickEvents();
+    makeEditable('a[id*=edit]');
+    makeDeleteable('a[id*=delete]');
+    makeDeleteable('.section_remove', 'sections');
 
-	    makeHelpable("img[class*=help-]");
-	    setupTabs('a[id*=tab-]');
-	    makeAddSection('a[id="add_section"]');
+    setupAllColorboxes();
+    setupMiscEvents();
+    setupMiscClickEvents();
+
+    makeHelpable("img[class*=help-]");
+    setupTabs('a[id*=tab-]');
+    makeAddSection('a[id="add_section"]');
 
 
-	    mark_as_linked();
+    mark_as_linked();
 
-	    mark_as_favorite();
+    mark_as_favorite();
 
-	  //  activateFindboxSearch();
-	    
-	    var rs = getResourceList();
-	    rs.init();
-	    var fdbx = getFindBoxSearch();
-	    fdbx.init();
-	    var guide = getGuideBase();
-	    guide.init();
-	    
+    // Append an intital section
 
-	    
-	    // Append an intital section
+    if ($('[id^=section]').length) {
 
-	    if ($('[id^=section]').length) {
+    } else {
 
-	    } else {
-	        
-		if (document.URL.indexOf('guide.php') > 0) {
+        if (document.URL.indexOf('guide.php') > 0) {
 
-		    $.ajax({
-			url: "helpers/section_data.php",
-			type: "POST",
-			data: { action : 'create' },
-			dataType: "html",
-			success: function(html) {
-			    $('div#tabs-0').append(html);
-			}
-		    });
-		}
+            $.ajax({
+                url: "helpers/section_data.php",
+                type: "POST",
+                data: { action: 'create' },
+                dataType: "html",
+                success: function (html) {
+                    $('div#tabs-0').append(html);
+                }
+            });
+        }
 
-	    }
+    }
 
-	    makeSectionSlider('div[id^="slider_section"]');
+    makeSectionSlider('div[id^="slider_section"]');
 
-	    $(".box-item").on('drag', function()
-			      {
-	    			  //$('#box_options').hide();
-			      });
+    $(".box-item").on('drag', function () {
+        //$('#box_options').hide();
+    });
 
-	    $(".draggable").draggable({
-		helper: 'clone', // Use a cloned helper
-		appendTo: 'body', // Append helper to body so you can hide the parent
-		start: function(){
-		    // Make the original transparent to avoid re-flowing of the elements
-		    // This makes the illusion of dragging the original item
-		    $(this).css({opacity:0});
-		},
-		stop: function(){
-		    // Show original after dragging stops
-		    $(this).css({opacity:1});
+    $(".draggable").draggable({
+        helper: 'clone', // Use a cloned helper
+        appendTo: 'body', // Append helper to body so you can hide the parent
+        start: function () {
+            // Make the original transparent to avoid re-flowing of the elements
+            // This makes the illusion of dragging the original item
+            $(this).css({ opacity: 0 });
+        },
+        stop: function () {
+            // Show original after dragging stops
+            $(this).css({ opacity: 1 });
 
-		}
-	    });
-	    
+        }
+    });
 
-	    $(".box-item")
-	    .mousedown(function() {
-	    	makeDropable(".dropspotty");
-		    console.log('make droppable');
-		    makeDropable(".cke");
-		    makeSortable(".sort-column");
-		    makeSortable(".sptab", 'sections');
-		    makeDraggable(".draggable");
-	    });
-	    
+
+    $(".box-item")
+    .mousedown(function () {
+        makeDropable(".dropspotty");
+        console.log('make droppable');
+        makeDropable(".cke");
+        makeSortable(".sort-column");
+        makeSortable(".sptab", 'sections');
+        makeDraggable(".draggable");
+    });
+
 });
-	
-
-
-// End $ within document.ready
