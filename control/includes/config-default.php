8edfc428e0ceeafc78798bb5c69ea2ed1e6a2209
agdarby <agdarby@gmail.com>
2015-11-03 16:14:07 -0500

    removed a couple of errant close parens which were busting the installation process

--- control/includes/config-default.php
+++ control/includes/config-default.php
@@ -60,13 +60,13 @@ $require_user_columns = array( "title", "classification", "tel", "address", "cit
 
 //used to declare which pluslets are activated in new box drop-down
 
-$pluslets_activated = array( "Basic", "Heading", "Feed", "TOC", "HTML5Video", "2", "4", "WorldCat", "Catalog", "Related", "ArticlesPlus", "SocialMedia", "SubjectSpecialist", "GoogleBooks", "GoogleScholar", "GoogleSearch", "1", "GuideSearch", "GuideSelect", "NewDBs", "NewGuides"));
+$pluslets_activated = array( "Basic", "Heading", "Feed", "TOC", "HTML5Video", "2", "4", "WorldCat", "Catalog", "Related", "ArticlesPlus", "SocialMedia", "SubjectSpecialist", "GoogleBooks", "GoogleScholar", "GoogleSearch", "1", "GuideSearch", "GuideSelect", "NewDBs", "NewGuides");
 
 
 // These are used to allow the admin to set different 'themes' for the pluslet titlebar
 // which the guide author may choose from
 
-$titlebar_styles = array( "White/Black" => "ts-whiteblack", "Black/White" => "ts-blackwhite"));
+$titlebar_styles = array( "White/Black" => "ts-whiteblack", "Black/White" => "ts-blackwhite");
 
 
 // SerSol provided disciplines; you may use for SerSol purposes, or add your own
