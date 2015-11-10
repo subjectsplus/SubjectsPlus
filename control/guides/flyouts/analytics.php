9535564e8a9f30a576db2b53f1baff8eb7c6f689
James Little <j.little@miami.edu>
2015-11-10 13:05:39 -0500

    Added a message if there haven't been any anayltics recorded.

--- control/guides/flyouts/analytics.php
+++ control/guides/flyouts/analytics.php
@@ -10,7 +10,7 @@ font-size: 2em;
 }
 
 .total-views-header {
-
+display:none;
 
 }
 </style>
@@ -20,7 +20,7 @@ font-size: 2em;
 	<h3><?php print _("Analytics"); ?></h3>
 
 	<div class="analytics_display">
-
+		<h2 class="no-analytics"><?php echo _("You don't have any recorded analytics."); ?></h2>
 		<h1 class="total-views-header">Total Views:</h1>
 		<span class="total-views-count"></span> </br> 
 			<span class="tab-click-header">Clicks on Tabs:</span>
