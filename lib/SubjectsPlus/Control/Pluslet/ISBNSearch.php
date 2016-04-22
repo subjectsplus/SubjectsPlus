6c9218c227de3ae39f663b244968eeacdbf79b26
Eric Bris <eric.bris@kreatetechnology.com>
2016-04-21 21:24:20 -0400

    Update ISBNSearch.php and .html

--- lib/SubjectsPlus/Control/Pluslet/ISBNSearch.php
+++ lib/SubjectsPlus/Control/Pluslet/ISBNSearch.php
@@ -7,6 +7,7 @@
  */
 
 namespace SubjectsPlus\Control;
+use SubjectsPlus\Control\Helpers\Searcher;
 require_once("Pluslet.php");
 
 class Pluslet_ISBNSearch extends Pluslet
@@ -20,27 +21,63 @@ class Pluslet_ISBNSearch extends Pluslet
     }
     protected function onEditOutput()
     {
+        $this->json_extra = json_decode($this->_extra);
+        $this->data = $data['totalItems'];
         $output = $this->loadHtml(__DIR__ . '/views/ISBNSearch.html');
-
+      
         $this->_body = $output;
     }
 
     protected function onViewOutput()
     {
-        $output = $this->loadHtml(__DIR__ . '/views/ISBNSearch.html');
+        /**
+         * @todo by Eric [Create an interface called ISearcher to encapsulate the logic to get the book data
+         * from an specific API, in this case google api, but in other case another different API]
+         * Validate that the used data exist and if not, jus define a default data.
+         * Get a way to get tha data as a batch from the Google or another API.
+         * Get the API we want to use from Dependency Injection.
+         */
+        $this->output_books = array();
+        if(isset($this->_extra) && $this->_extra !== "") {
+            $json_extra = json_decode($this->_extra);
+          
+           // $searcher = new SearcherFactory();
 
-        $this->_body = $output;
+            foreach ($json_extra as $key => $value) {
+             $page = file_get_contents("https://www.googleapis.com/books/v1/volumes?q=isbn:".$value);
+             $elements = json_decode($page);
+             $title = $elements->items[0]->volumeInfo->title;
+             $subtitle = isset($elements->items[0]->volumeInfo->subtitle)?$elements->items[0]->volumeInfo->subtitle:"";
+             $authors = $elements->items[0]->volumeInfo->authors;
+             $thumbnail = $elements->items[0]->volumeInfo->imageLinks->smallThumbnail;
+             
+             $this->output_books[] = array("title"=>$title, "subtitle"=>$subtitle, "authors"=>$authors, "image"=>$thumbnail);
+            }
+            
+
+
+             $output = $this->loadHtml(__DIR__ . '/views/ISBNSearchView.html');
+
+            $this->_body = $output;
+        }
 
     }
+
     static function getMenuName()
     {
         return _('ISBN Search');
     }
 
+    /**
+     * @todo by Eric [Define this method only in Pluslet parent class and pass the class to use and the icon name from children]
+     */
     static function getMenuIcon()
     {
         $icon="<i class=\"fa fa-users\" title=\"" . _("ISBN Search") . "\" ></i><span class=\"icon-text\">"  . _("ISBNSearch") . "</span>";
         return $icon;
+    }
+    private function checkISBN(){
+        return json_encode(array("succes"=>true));
     }
 
 }
