2099a8ec116af0fdf6aa57fbf6a7c131824f9e6b
cbrownroberts <charlesbrownroberts@miami.edu>
2016-05-03 14:55:22 -0400

    still working on setting body_content for subjectSpecialist

--- lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistEditOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistEditOutput.php
@@ -85,8 +85,7 @@
 
 
     //$this->_body .= $this->_editor;
-    $body_content = $this->_body_content[0]['body'];
-    if( (isset($body_content))  && ($body_content != '') ) {
+    if( (isset($this->_body_content[0]['body'])) && (is_string($this->_body_content[0]['body'])) && ($this->_body_content[0]['body'] != '') ) {
         $this->_body .= "<textarea cols='60' id='editor1' name='editor1' rows='10'>{$body_content}</textarea>";
     } else {
         $this->_body .= "<textarea cols='60' id='editor1' name='editor1' rows='10'></textarea>";
