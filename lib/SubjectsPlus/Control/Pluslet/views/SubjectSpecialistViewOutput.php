84e86d7522551bb242894aeed8d0dc6ad07c13be
cbrownroberts <charlesbrownroberts@miami.edu>
2016-05-18 13:22:09 -0400

    fix for subject specialist clone where clone has duplicate box settings and titlebar

--- lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistViewOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialistViewOutput.php
@@ -9,23 +9,15 @@
 
 <div>
     <?php echo $this->_body_content; ?>
-
-
 </div>
-
-
-<div id="hidden-pluslet-id" data-hidden-pluslet-id="<?php echo $this->_pluslet_id; ?>" style="display:none;"></div>
 
 <script>
 
-    var hiddenPlusletId = <?php echo $this->_pluslet_id; ?>;
+    if( $('[name="Clone"]') ) {
 
-
-    if( $('#pluslet-' + hiddenPlusletId).closest('[name="Clone"]').length > 0) {
-
-        $('#pluslet-' + hiddenPlusletId).children('.box_settings').remove();
-        $('#pluslet-' + hiddenPlusletId).children('.titlebar').remove();
+        var clone = $('[name="Clone"]');
+        var subjectSpecialists = $(clone).children().find('[name="SubjectSpecialist"]');
+        $(subjectSpecialists).children('.box_settings, .titlebar').remove();
     }
-
 
 </script>
