d3992632166667f505b20941d6c29427b31e8dbe
agdarby <agdarby@gmail.com>
2016-06-13 14:31:49 -0400

    tidying up the guides admin page

--- control/admin/manage_guides.php
+++ control/admin/manage_guides.php
@@ -48,13 +48,13 @@ $linkcheck_text = _("Check Links");
 if ($value[7] != "1") { $active = " <span style=\"font-size:smaller; color: #666;\">inactive</span>";} else { $active = ""; }
 
 $row_colour = ($row_count % 2) ? $colour1 : $colour2;
-$staff_list .= "<div class=\"$row_colour striper\" style=\"clear: both; float: left; min-width: 500px;\">
+$staff_list .= "<div class=\"$row_colour striper\" style=\"clear: both; float: left; width: 60%;\">
 <input type=\"checkbox\" name=\"guide-$value[0]\" value=\"$value[0]\"><a class=\"showmedium-reloader\" href=\"../guides/metadata.php?subject_id=$value[0]&wintype=pop\"><i class=\"fa fa-gear fa-lg\" title=\"$edit_text\" alt=\"$edit_text\"></i></a> 
 &nbsp;&nbsp; <a target=\"_blank\" href=\"../../subjects/guide.php?subject=$value[6]\"><i class=\"fa fa-eye fa-lg\" title=\"$view_text\" alt=\"$view_text\"></i></a> 
 &nbsp;&nbsp; <a class=\"showmedium\" href=\"../guides/link_checker.php?subject_id=$value[0]&wintype=pop\"><i class=\"fa fa-link fa-lg\" title=\"$linkcheck_text\" alt=\"$linkcheck_text\"></i></a> 
 &nbsp;&nbsp; <a href=\"../guides/guide.php?subject_id=$value[0]\">$value[1]</a> $active</div>
-<div class=\"$row_colour striper\" style=\"float: left; min-width: 100px;\">$value[2] $value[3]</div>
-<div class=\"$row_colour striper\" style=\"float: left; min-width: 75px;\">$value[5]</div>";
+<div class=\"$row_colour striper\" style=\"float: left; width: 15%; min-width: 100px;\">$value[2] $value[3]</div>
+<div class=\"$row_colour striper\" style=\"float: left; width: 15%; min-width: 75px;\">$value[5]</div>";
 $row_count++;
   }
 
