a52288bc6324f3395c03e24660af869a5a243a0e
cbrownroberts <charlesbrownroberts@miami.edu>
2016-07-14 15:03:42 -0400

    Record class for OAI to return subject and staff data by subject_id

--- oai/oai.php
+++ oai/oai.php
@@ -6,7 +6,9 @@
  * Time: 11:25 AM
  */
 
+use SubjectsPlus\Control\Querier;
 use SubjectsPlus\Control\OAI\Repo;
+use SubjectsPlus\Control\OAI\Record;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -14,7 +16,12 @@ ini_set('display_errors', 1);
 include_once ('../control/includes/autoloader.php');
 include_once('../control/includes/config.php');
 
-$setup = array('repositoryName' => $resource_name, 'baseUrl' =>$BaseURL.'oai/oai.php', 'adminEmail' => $administrator_email);
+$setup = array('repositoryName'   => $resource_name,
+                'baseUrl'         => $BaseURL.'oai/oai.php',
+                'adminEmail'      => $administrator_email,
+                'institutionName' => $institution_name,
+                'identifierUrl'   => $BaseURL.'subjects/guide.php?subject_id=');
+
 $repo = new Repo(new XSLTProcessor(),$setup);
 
 if ($_GET['verb'] == 'Identify') {
@@ -29,3 +36,18 @@ if ($_GET['verb'] == 'ListMetadataFormats') {
     $dom->load('./xsl/ListMetadataFormats.xsl');
     echo $repo->listMetadataFormats($dom);
 }
+
+
+if( (isset($_GET['subject_id'])) && ($_GET['verb'] == 'GetRecord') ) {
+
+    $id = $_GET['subject_id'];
+
+    $db = new Querier();
+
+    $record = new Record($db, $setup);
+    $record->getRecord($id);
+    $record->toArray();
+
+    var_dump($record);
+}
+
