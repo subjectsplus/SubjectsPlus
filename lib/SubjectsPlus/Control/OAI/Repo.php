94c9996e4bd596f424fc6d4ba36fc2d4c56ce65a
James Little <j.little@miami.edu>
2016-07-14 12:36:01 -0400

    Inital OAI commit

---
+++ lib/SubjectsPlus/Control/OAI/Repo.php
@@ -0,0 +1,38 @@
+<?php
+namespace SubjectsPlus\Control\OAI;
+use DOMDocument;
+use XSLTProcessor;
+
+/**
+ * Created by PhpStorm.
+ * User: jlittle
+ * Date: 7/14/16
+ * Time: 12:12 PM
+ */
+class Repo
+{
+
+    private $repositoryName;
+    private $baseUrl;
+    private $adminEmail;
+    private $xslt;
+    
+    public function __construct(XSLTProcessor $xslt, array $setup)
+    {
+        $this->xslt = $xslt;
+        $this->repositoryName = $setup['repositoryName'];
+        $this->baseUrl = $setup['baseUrl'];
+        $this->adminEmail = $setup['adminEmail'];
+    }
+
+    public function identify(DOMDocument $xsl)
+    {
+        $this->xslt->importStylesheet($xsl);
+        $this->xslt->setParameter('','responseDate',date('c'));
+        $this->xslt->setParameter('','repositoryName',$this->repositoryName);
+        $this->xslt->setParameter('','baseUrl',$this->baseUrl);
+        $this->xslt->setParameter('','adminEmail',$this->adminEmail);
+        return  $this->xslt->transformToXml($xsl);
+    }
+
+}
---
+++ oai/oai.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlittle
+ * Date: 7/14/16
+ * Time: 11:25 AM
+ */
+
+use SubjectsPlus\Control\OAI\Repo;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once ('../control/includes/autoloader.php');
+include_once('../control/includes/config.php');
+
+$setup = array('repositoryName' => $resource_name, 'baseUrl' =>$BaseURL.'oai/oai.php', 'adminEmail' => $administrator_email);
+$repo = new Repo(new XSLTProcessor(),$setup);
+
+if ($_GET['verb'] == 'Identify') {
+    $dom = new DOMDocument();
+    $dom->load('./xsl/Identify.xsl');
+    echo $repo->identify($dom);
+}
