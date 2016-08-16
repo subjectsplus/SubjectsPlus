49a86cf13b369de72c3dbb03cdb19bac54a45d1e
pvillanueva01 <pvillanueva@libwet238.local>
2016-08-16 13:01:31 -0400

    one more file

--- assets/js/guides/GuideCollectionService.js
+++ assets/js/guides/GuideCollectionService.js
@@ -9,7 +9,7 @@ function guideCollectionService() {
     var myGuideCollection = {
 
         settings : {
-            collectionActionUrl : "../../control/guides/helpers/collections.php?",
+            collectionActionUrl : "/control/guides/helpers/collections.php?",
             sortableGuideList : $('ul#guide-list')
         },
         strings : {
