793ca8231823c09e9dd3efe62cd92c0fc9c9cfba
pvillanueva01 <pvillanueva@libwet238.local>
2016-08-23 17:40:02 -0400

    Initial styles for subject specialist

--- lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialist.php
+++ lib/SubjectsPlus/Control/Pluslet/views/SubjectSpecialist.php
@@ -17,27 +17,29 @@
 ?>
 
 <div class="subject-specialists" data-staff-id="<?php echo $this->staff_id; ?>">
-    <h4 data-show-name="<?php echo $this->showName; ?>"><?php echo $this->fullname; ?></h4>
-    <div data-show-photo="<?php echo $this->showPhoto; ?>" ><img src="<?php echo $this->img_url; ?>" /> </div>
-
-    <ul class="staff-details">
-        <li class="staff-content" data-show-title="<?php echo $this->showTitle; ?>"><?php echo $this->title; ?></li>
-        <li class="staff-content" data-show-email="<?php echo $this->showEmail; ?>"><?php echo $this->email; ?></li>
-        <li class="staff-content" data-show-phone="<?php echo $this->showPhone; ?>">(<?php echo $this->tel_prefix; ?>) <?php echo $this->tel; ?></li>
-    </ul>
-    <div class="social-details">
-        <div class="staff-social" data-show-facebook="<?php echo $this->showFacebook; ?>">
-            <a href="https://facebook.com/<?php echo $this->facebook; ?>"><i class="fa fa-facebook"></i></a>
-        </div>
-        <div class="staff-social" data-show-twitter="<?php echo $this->showTwitter ; ?>">
-            <a href="https://twitter.com/<?php echo $this->twitter; ?>"><i class="fa fa-twitter"></i></a>
-        </div>
-        <div class="staff-social" data-show-pinterest="<?php echo $this->showPinterest; ?>">
-            <a href="https://pinterest.com/<?php echo $this->pinterest; ?>"><i class="fa fa-pinterest"></i></a>
-        </div>
-        <div class="staff-social" data-show-instagram="<?php echo $this->showInstagram; ?>">
-            <a href="https://instagram.com/<?php echo $this->instagram; ?>"><i class="fa fa-instagram"></i></a>
-        </div>
+    
+    <div class="specialist-photo" data-show-photo="<?php echo $this->showPhoto; ?>" ><img src="<?php echo $this->img_url; ?>" /> </div>
+    <div class="specialist-info">
+        <h4 data-show-name="<?php echo $this->showName; ?>"><?php echo $this->fullname; ?></h4>
+        <ul class="staff-details">
+            <li data-show-title="<?php echo $this->showTitle; ?>"><?php echo $this->title; ?></li>
+            <li data-show-email="<?php echo $this->showEmail; ?>"><a href="mailto:<?php echo $this->email; ?>"><?php echo $this->email; ?></a></li>
+            <li data-show-phone="<?php echo $this->showPhone; ?>">(<?php echo $this->tel_prefix; ?>) <?php echo $this->tel; ?></li>       
+            <li>
+                <div class="staff-social" data-show-facebook="<?php echo $this->showFacebook; ?>">
+                    <a href="https://facebook.com/<?php echo $this->facebook; ?>"><i class="fa fa-facebook"></i></a>
+                </div>
+                <div class="staff-social" data-show-twitter="<?php echo $this->showTwitter ; ?>">
+                    <a href="https://twitter.com/<?php echo $this->twitter; ?>"><i class="fa fa-twitter"></i></a>
+                </div>
+                <div class="staff-social" data-show-pinterest="<?php echo $this->showPinterest; ?>">
+                    <a href="https://pinterest.com/<?php echo $this->pinterest; ?>"><i class="fa fa-pinterest"></i></a>
+                </div>
+                <div class="staff-social" data-show-instagram="<?php echo $this->showInstagram; ?>">
+                    <a href="https://instagram.com/<?php echo $this->instagram; ?>"><i class="fa fa-instagram"></i></a>
+                </div>
+            </li>
+        </ul>
     </div>
 </div>
 
