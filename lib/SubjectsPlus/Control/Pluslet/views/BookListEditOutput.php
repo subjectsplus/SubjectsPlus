358e9224db01a23339cbe7f1f63272d6318bbb67
abelfacenda <abelfacenda26@gmail.com>
2016-09-06 13:22:03 -0400

    Book List Pluslet Views

---
+++ lib/SubjectsPlus/Control/Pluslet/views/BookListEditOutput.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: acarrasco
+ * Date: 8/23/2016
+ * Time: 2:50 PM
+ */?>
+
+        <textarea rows="4" cols="30" name="BookList-extra-isbn"
+                  placeholder="Please insert a comma-separated ISBNs list"><?php
+                if ($this->_extra != null) {
+                        echo $this->_extra['isbn'];
+                }
+            ?></textarea>
+
+<script>
+    var b = bookList();
+    b.init();
+</script>
---
+++ lib/SubjectsPlus/Control/Pluslet/views/BookListViewOutput.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: acarrasco
+ * Date: 8/23/2016
+ * Time: 2:52 PM
+ */?>
+
+<div class="booklist-container">
+    <input class="booklist-isbn-list-input" type="hidden" value="<?php echo $this->_extra['isbn']?>"/>
+    <input class="booklist-syndetics-client-code" type="hidden" value="<?php global $syndetics_client_code; echo (isset($syndetics_client_code)) ? $syndetics_client_code : '';?>"/>
+</div>
+
+<script>
+    var b = bookList();
+    b.init();
+
+
+</script>
