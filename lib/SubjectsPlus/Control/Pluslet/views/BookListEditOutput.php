f0599bcbb73e6db1b1f02b53d5af122ff8193754
abelfacenda <abelfacenda26@gmail.com>
2016-10-26 14:32:25 -0400

    Added description field to book list pluslet

--- lib/SubjectsPlus/Control/Pluslet/views/BookListEditOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/BookListEditOutput.php
@@ -6,6 +6,13 @@
  * Time: 2:50 PM
  */?>
 
+<textarea rows="4" cols="30" name="BookList-extra-listDescription"
+          placeholder="Please insert a description for this list"><?php
+    if ($this->_extra != null) {
+        echo $this->_extra['listDescription'];
+    }
+    ?></textarea>
+
 <textarea rows="4" cols="30" name="BookList-extra-isbn"
           placeholder="Please insert a comma-separated ISBNs list"><?php
     if ($this->_extra != null) {
