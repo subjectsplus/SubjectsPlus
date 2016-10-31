6f40887a0568052b4a37b7f05805cc54f1494e42
abelfacenda <abelfacenda26@gmail.com>
2016-10-31 16:14:35 -0400

    Added Book List pluslet Primo vars

--- lib/SubjectsPlus/Control/Pluslet/views/BookListViewOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/BookListViewOutput.php
@@ -11,9 +11,9 @@
         <input class="booklist-isbn-list-input" type="hidden" value="<?php echo $this->_extra['isbn']?>"/>
         <input class="booklist-syndetics-client-code" type="hidden" value="<?php global $syndetics_client_code; echo (isset($syndetics_client_code)) ? $syndetics_client_code : '';?>"/>
         <input class="google-books-api-key" type="hidden" value="<?php global $google_books_api_key; echo (isset($google_books_api_key)) ? $google_books_api_key : '';?>"/>
-        <input class="primo-domain" type="hidden" value="<?php global $primo_domain; echo (isset($primo_domain)) ? $primo_domain : '';?>"/>
-        <input class="primo-institution-code" type="hidden" value="<?php global $primo_institution_code; echo (isset($primo_institution_code)) ? $primo_institution_code : '';?>"/>
-        <input class="primo-view" type="hidden" value="<?php global $primo_vid; echo (isset($primo_vid)) ? $primo_vid : '';?>"/>
+        <input class="primo-domain" type="hidden" value="<?php global $booklist_primo_domain; echo (isset($booklist_primo_domain)) ? $booklist_primo_domain : '';?>"/>
+        <input class="primo-institution-code" type="hidden" value="<?php global $booklist_primo_institution_code; echo (isset($booklist_primo_institution_code)) ? $booklist_primo_institution_code : '';?>"/>
+        <input class="primo-view" type="hidden" value="<?php global $booklist_primo_vid; echo (isset($booklist_primo_vid)) ? $booklist_primo_vid : '';?>"/>
 
         <p class="booklist-list-description"><?php  echo isset($this->_extra['listDescription']) ? $this->_extra['listDescription'] : '';?></p>
     </div>
