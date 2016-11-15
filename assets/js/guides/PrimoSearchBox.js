9e0fc4d2f76c531c1a8644e6b4c4ed62ee757b4a
cbrownroberts <charlesbrownroberts@miami.edu>
2016-11-15 13:08:43 -0500

    added search_scope param to PrimoSearchBox so that the search works for both catalog and everything

--- assets/js/guides/PrimoSearchBox.js
+++ assets/js/guides/PrimoSearchBox.js
@@ -15,10 +15,12 @@ function primoSearchBox() {
         bindUiActions: function () {
             myPrimoSearchBox.toggleMoreOptions();
             myPrimoSearchBox.search();
+
         },
         init: function () {
             myPrimoSearchBox.bindUiActions();
             myPrimoSearchBox.hideMoreOptions();
+            myPrimoSearchBox.toggleSearchScope();
         },
 
         search : function () {
@@ -28,7 +30,7 @@ function primoSearchBox() {
                 if(myPrimoSearchBox.settings.rtype.val() && myPrimoSearchBox.settings.precisionOperator.val()) {
                     myPrimoSearchBox.settings.primoQuery.val(myPrimoSearchBox.settings.rtype.val() + ',' +
                                             myPrimoSearchBox.settings.precisionOperator.val() + ',' +
-                                            myPrimoSearchBox.settings.primoQueryTemp.val().replace(/[,]/g, " "));
+                                            myPrimoSearchBox.settings.primoQueryTemp.val().replace(/[,]/g, " ") );
                 } else {
                     myPrimoSearchBox.settings.primoQuery.val('any,contains,' + myPrimoSearchBox.settings.primoQueryTemp.val().replace(/[,]/g, " "));
                 }
@@ -44,6 +46,19 @@ function primoSearchBox() {
         toggleMoreOptions : function () {
             myPrimoSearchBox.settings.advSearchBtn.on('click', function () {
                 myPrimoSearchBox.settings.advancedSearchContainer.toggle();
+            });
+        },
+
+        toggleSearchScope: function () {
+            $('body').on('change', 'select[name="tab"]', function () {
+
+                var search_scope = $(this).val();
+
+                if( search_scope == 'default_tab') {
+                    $('.search_scope').val('default_scope');
+                } else {
+                    $('.search_scope').val('Everything');
+                }
             });
         }
     };
