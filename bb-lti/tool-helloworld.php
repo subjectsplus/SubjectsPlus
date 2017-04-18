ff5900299ce312ec9527a6e28395ba4578c6cb9c
abelfacenda <abelfacenda26@gmail.com>
2017-04-18 10:07:22 -0400

    gender studies guide

--- bb-lti/tool-helloworld.php
+++ bb-lti/tool-helloworld.php
@@ -26,7 +26,7 @@ if ($lti->valid) {
     $course_id = $_REQUEST["context_label"];
 
     if (strcmp($course_id, '123456') == 0){
-        header("Location: http://sp.library.miami.edu/subjects/english"); /* Redirect browser */
+        header("Location: http://sp.library.miami.edu/subjects/guide.php?subject=WomensGenderStudies"); /* Redirect browser */
         exit();
     }else{
         header("Location: http://sp.library.miami.edu/subjects"); /* Redirect browser */
