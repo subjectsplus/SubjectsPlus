a6a720d10f435f00298b2d9d5859acbe29aa4e3c
abelfacenda <abelfacenda26@gmail.com>
2018-02-07 14:49:48 -0500

    check if lti enabled

--- addons/bb-lti/lti.php
+++ addons/bb-lti/lti.php
@@ -1,37 +1,44 @@
 <?php
 try {
-    if (required_indexes_exist()) {
-        include("../../control/includes/autoloader.php"); // need to use this if header not loaded yet
-        include("../../control/includes/config.php");
-        include_once('../../control/includes/functions.php');
-        require_once '../../lib/ims-blti/blti.php';
-        require_once('../../control/lti_controller/LTICourseController.php');
+    global $lti_enabled;
 
-        global $lti_secret;
-        $lti = new BLTI($lti_secret, false, false);
-        session_start();
+    if (isset($lti_enabled)){
+        if ($lti_enabled){
+            if (required_indexes_exist()) {
+                include("../../control/includes/autoloader.php"); // need to use this if header not loaded yet
+                include("../../control/includes/config.php");
+                include_once('../../control/includes/functions.php');
+                require_once '../../lib/ims-blti/blti.php';
+                require_once('../../control/lti_controller/LTICourseController.php');
 
-        if ($lti->valid) {
+                global $lti_secret;
+                $lti = new BLTI($lti_secret, false, false);
+                session_start();
 
-            // let's use our Pretty URLs if mod_rewrite = TRUE or 1
-            if ($mod_rewrite == 1) {
-                $guide_path = "";
+                if ($lti->valid) {
+
+                    // let's use our Pretty URLs if mod_rewrite = TRUE or 1
+                    if ($mod_rewrite == 1) {
+                        $guide_path = "";
+                    } else {
+                        $guide_path = $PublicPath;
+                    }
+                    $course_label = scrubData($_REQUEST["context_label"]);
+
+                    $courses_code = new LTICourseController('bb_course_code', 'bb_course_instructor');
+
+                    $courses_code->processCourseCode($course_label, $guide_path);
+                }else{
+                    die('Invalid LTI call');
+                    exit();
+                }
             } else {
-                $guide_path = $PublicPath;
+                die('Invalid LTI call');
+                exit();
             }
-            $course_label = scrubData($_REQUEST["context_label"]);
-
-            $courses_code = new LTICourseController('bb_course_code', 'bb_course_instructor');
-
-            $courses_code->processCourseCode($course_label, $guide_path);
-        }else{
-            die('Invalid LTI call');
-            exit();
         }
-    } else {
-        die('Invalid LTI call');
-        exit();
     }
+
 } catch (Exception $e) {
     echo 'Exception "\n"', $e->getMessage(), "\n";
     exit();
