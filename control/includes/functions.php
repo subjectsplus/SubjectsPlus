53a58b137543d493f41eab5a2baeb6b3c7a11a62
abelfacenda <abelfacenda26@gmail.com>
2018-02-09 15:19:37 -0500

    updated db query to filter databases by subjects

--- control/includes/functions.php
+++ control/includes/functions.php
@@ -339,23 +339,18 @@ function getDBbySubBoxes($selected_sub, $additionaltype = "Placeholder") {
 
   //$subs_query = "SELECT distinct subject_id, subject, type FROM `subject` WHERE (type = 'Subject' " . $morequery . ") AND active = '1' ORDER BY subject";
 
-   $subs_query = "
-SELECT DISTINCT s.subject_id, s.subject, s.type
-FROM title AS t
-INNER JOIN location_title AS lt
-ON t.title_id = lt.title_id
-INNER JOIN location AS l
-ON lt.location_id = l.location_id
-INNER JOIN restrictions AS r
-ON l.access_restrictions = r.restrictions_id
-INNER JOIN rank AS rk
-ON rk.title_id = t.title_id
-INNER JOIN subject AS s
-ON rk.subject_id = s.subject_id
+    $subs_query = "SELECT s.subject_id, s.subject, s.type
+FROM subject as s WHERE exists(
+SELECT t.title, l.record_status, r.title_id, r.rank_id, r.description_override
+FROM rank r, location_title lt, location l, title t
+    WHERE subject_id = s.subject_id
+    AND lt.title_id = r.title_id
+    AND l.location_id = lt.location_id
+    AND t.title_id = lt.title_id
+    AND l.eres_display = 'Y'
+    AND l.record_status = 'Active'
+    AND r.dbbysub_active = 1)
 AND s.active = 1
-AND s.type = 'Subject'
-AND eres_display = 'Y'
-AND dbbysub_active = 1
 ORDER BY s.subject";
   $subs_result = $db->query($subs_query);
 
