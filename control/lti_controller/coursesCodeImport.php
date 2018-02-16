ac18b3f4deb92e891093c906b046ac4339a84986
abelfacenda <abelfacenda26@gmail.com>
2018-02-16 17:10:28 -0500

    version control of lti_controller config file

--- control/lti_controller/coursesCodeImport.php
+++ control/lti_controller/coursesCodeImport.php
@@ -1,6 +1,19 @@
 <?php
-include("../includes/autoloader.php"); // need to use this if header not loaded yet
-include("../includes/config.php");
+//Create a config.php file in the same directory where coursesCodeImport.php file is
+// The config.php file needs to contain the following code
+
+//$server_environment = getcwd();
+//
+//switch($server_environment) {
+//
+//    case '/path/to/lti_controller/directory':
+//        define('SP_PATH', '/path/to/local/sp4/directory');
+//        break;
+//}
+
+include('config.php');
+include(SP_PATH . "/control/includes/autoloader.php"); // need to use this if header not loaded yet
+include(SP_PATH . "/control/includes/config.php");
 require_once('LTICourseController.php');
 
 try {
