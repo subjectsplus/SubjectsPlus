a7a6665564f9546adb5f4621b830e77a89ac65f4
abelfacenda <abelfacenda26@gmail.com>
2018-03-22 10:56:29 -0400

    rolling back changes on functions.php

--- control/includes/functions.php
+++ control/includes/functions.php
@@ -967,7 +967,7 @@ function getDBbyTypeBoxes($selected_type = "", $show_formats = TRUE) {
   return $alphabet;
 }
 
-function getLetters($table, $selected = "A", $numbers = 1, $show_formats = TRUE, $return_az_range = FALSE) {
+function getLetters($table, $selected = "A", $numbers = 1, $show_formats = TRUE) {
 
   $selected = scrubData($selected);
 
@@ -1032,9 +1032,6 @@ function getLetters($table, $selected = "A", $numbers = 1, $show_formats = TRUE,
     }
   }
 
-  if ($return_az_range){
-      return $azRange;
-  }
 
   $alphabet = "<div id=\"letterhead\" align=\"center\">";
 
