37c3ef8f7d3f227746c467c0bafdf7d42809a1bb
pvillanueva01 <pvillanueva@libwet238.local>
2018-06-22 15:09:56 -0400

    404 page, additional styles

--- subjects/themes/um-new/blank-404.php
+++ subjects/themes/um-new/blank-404.php
@@ -1,9 +1,7 @@
 <?php
 $page_title = "Page Not Found (404)";
-$description = "404 Page not found";
+$description = "404 Page Not Found";
 $keywords = "404, missing page";
-
-//$use_jquery = array("sp_legacy");
 
 include("../../../control/includes/config.php");
 include("../../../control/includes/functions.php");
@@ -12,35 +10,73 @@ include("../../../control/includes/functions.php");
 include("includes/header_um-new.php");
 ?>
 
+<div class="second-level">
+    <section class="page-header">
+        <div class="row no-gutters">
+            <div class="col-lg-6 order-last order-lg-first">
+                <div class="page-header-details">
+                    <div class="page-header-details-block">
+                        <h1>Page Not Found (404)</h1>
+                        <hr align="center" class="hr-panel">
+                        <ul class="d-sm-flex flex-sm-row flex-sm-nowrap justify-content-sm-center">
 
-    <div class="panel-container">
-        <div class="pure-g">
 
-            <div class="pure-u-1 pure-u-lg-3-4 panel-adj">
-                <div class="breather">
-                    <p>We looked everywhere, and we can't find that page.</p>
-                    <p>Try <strong>searching</strong> for the page you want, or
-                        <strong> select a category</strong> from the navigational menu above.</p>
-                    <br>
-                    <form id="head_search" method="post" action="http://library.miami.edu/wp-content/themes/umiami/resolver.php">
-                        <input type="hidden" value="website" name="searchtype">
-                        <input id="search_tabs" class="searchinput-4" type="text" autocomplete="off" size="40" name="searchterms" value="">
-                        <input type="submit" name="submitsearch" value="Search" class="search404">
-                    </form>
+
+                            <li><i class="fas fa-envelope"></i> <a href="mailto:webmaster.lib@miami.edu">webmaster.lib@miami.edu</a></li>
+
+                        </ul>
+
+
+                        <div class="page-highlights">
+
+                        </div>
+
+
+                        <div class="favorite-heart">
+                            <div id="heart" title="Add to Favorites" tabindex="0" role="button" data-type="favorite-page-icon"
+                                 data-item-type="Pages" alt="Add to My Favorites" class="uml-quick-links favorite-page-icon" ></div>
+                        </div>
+                    </div>
                 </div>
-            </div> <!--end 3/4 main area column-->
+            </div>
+            <div class="col-lg-6 order-first order-lg-last">
+                <div class="page-header-img" style="background-image: url('https://uml-e-wpapi.azurewebsites.net/wp-content/uploads/2018/04/1260-450-nasa-control.jpg');">
 
+                    <script>
+                        //Basic Image Tooltips
+                        $( function(){
+                            $('.basic-img-info').tooltip();
+                        });
+                    </script>
+                    <a href="" class="no-decoration basic-img-info" data-toggle="tooltip" data-placement="top" title="Project Gemini Mission Control Center"><i class="fas fa-info-circle"></i></a>
 
-            <div class="pure-u-1 pure-u-lg-1-4 database-page sidebar-bkg">
+                </div>
+            </div>
+        </div>
 
-            </div><!--end 1/4 sidebar column-->
+    </section>
+</div>
 
+<section class="search-area d-none d-lg-block">
+    <div class="full-search">
+        <div class="container text-center">
+            <div class="search-group">
+                <div id="uml-site-search-container"></div>
+                <div class="adv-search d-none">
+                    <a class="no-decoration default" href="#">Advanced Search</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-
-
-
-        </div><!--end pure-g-->
-    </div> <!--end panel-container-->
+<section class="section">
+    <div class="container">
+        <h3>Oh, no!  This page doesn&#8217;t exist.</h3>
+        <p>We&#8217;re sorry you had to see this.  We looked everywhere, and we can't find that page.</p>
+        <p>Try <strong>searching</strong> for the page you want, or <strong>select a category</strong> from the navigational menu above.</p>
+    </div>
+</section>
 
 <?php
 // Load footer file
