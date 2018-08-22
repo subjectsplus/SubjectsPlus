8bfef1883cd0a04a61aff2229d1d349cd4a9dac1
Charles <cgb37@miami.edu>
2018-08-22 12:06:48 -0400

    bare minimum for healthcheck page - include config only

--- healthcheck.php
+++ healthcheck.php
@@ -8,10 +8,3 @@
 
 
 include("control/includes/config.php");
-include("control/includes/functions.php");
-
-$page_title = "SubjectsPlus:  Health Check";
-$description = "The best stuff for your research.  No kidding.";
-$keywords = "research, databases, subjects, search, find, suggestion box, talkback, staff list, faqs";
-
-//include("subjects/includes/header.php");
