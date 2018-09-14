7cd2404725b17c0133906a96921782e13054e76b
pvillanueva01 <pvillanueva@libwet238.local>
2018-09-14 16:46:47 -0400

    remove code

--- subjects/includes/header_um-new.php
+++ subjects/includes/header_um-new.php
@@ -23,48 +23,11 @@
         echo "<div id='google-analytics-ua' style='visibility: hidden;' data-uacode='{$google_analytics_ua}'></div>";
         echo "<div id='google_tag_manager' style='visibility: hidden;' data-tag-manager='{$google_tag_manager}'></div>";
 
-        //if( file_exists('includes/google-analytics-tag-manager.php') ) {
-         //   include_once ('google-analytics-tag-manager.php');
-        //}
+        if( file_exists('includes/google-analytics-tag-manager.php') ) {
+            include_once ('google-analytics-tag-manager.php');
+        }
     }
     ?>
-
-    <!-- Google Analytics Tracker Code-->
-    <script>
-        var uaCode = $('#google-analytics-ua').attr('data-uacode');
-        //console.log(uaCode);
-
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'uaCode', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-
-    <!-- Google Analytics Tag Manager-->
-    <script>
-        dataLayer = [];
-        var gaTagManager = $('#google_tag_manager').attr('data-tag-manager');
-
-    </script>
-
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'gaTagManager');</script>
-    <!-- End Google Tag Manager -->
 </head>
 <body>
 
