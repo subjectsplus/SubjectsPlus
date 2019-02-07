389f4f591333ac17d49435d884767994f12c7f4d
Charles <charlesbrownroberts@gmail.com>
2019-02-07 13:58:34 -0500

    fix: js.php needed to be included twice causing javascript problems. split js.php into js_vendor.php and js_custom.php and put vendor in head and custom in footer

--- control/includes/guide_footer.php
+++ control/includes/guide_footer.php
@@ -4,16 +4,5 @@
 <!--end wrap div -->
 </div>
 
-
 </body>
-
-<script>
-	// Initialize the guide interface 
-	var myGuideSetup = guideSetup();
-	myGuideSetup.init();
-
-    var ss = subjectSpecialist();
-    ss.init();
-
-</script>
 </html>
