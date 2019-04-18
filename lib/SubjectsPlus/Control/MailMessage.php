2e24d8568f5d87fbead861bdd0dd22abfcc1b2cf
Charles <charlesbrownroberts@gmail.com>
2019-04-18 16:25:46 -0400

    adding PHPMailer to custom Mailer class

--- lib/SubjectsPlus/Control/MailMessage.php
+++ lib/SubjectsPlus/Control/MailMessage.php
@@ -1,73 +1,193 @@
 <?php
    namespace SubjectsPlus\Control;
+   //Import the PHPMailer class into the global namespace
+   use PHPMailer\PHPMailer\PHPMailer;
 class MailMessage {
 
-    private $_to;
-    private $_from;
-    private $_fromString;
-    private $_subjectLine;
-    private $_content;
-    private $_header;
 
-    public function __construct($params=NULL) {
-        if (is_array($params)) {
-            if (isset($params['to'])) {
-                $this->_to = $params['to'];
-            }
-            if (isset($params['subjectLine'])) {
-                $this->_subjectLine = $params['subjectLine'];
-            }
-            if (isset($params['content'])) {
-                $this->_content = $params['content'];
-            }
-            if (isset($params['from'])) {
-                $this->_from = $params['from'];
-                $this->_fromString = _("Library_No_Reply") . "<" . $this->_from . ">";
-                $this->_header = "Return-Path: " . $this->_from . "\n";
-                $this->_header .= "From:  " . $this->_from . "\n";
-                $this->_header .= "Content-Type: text/html; charset=iso-8859-1;\n";
-            }
-        }
-    }
+	private $_setFrom;
+	private $_addReplyTo;
+	private $_addAddress;
+	private $_Subject;
+	private $_msgHTML;
+	private $_AltBody;
+	private $_addAttachment;
 
-    public function getTo() {
-        return $this->_to;
-    }
 
-    public function getSubjectLine() {
-        return $this->_subjectLine;
-    }
 
-    public function getContent() {
-        return $this->_content;
-    }
+	public function __construct() {
 
-    public function getFrom() {
-        return $this->_fromString;
-    }
+		//SMTP needs accurate times, and the PHP time zone MUST be set
+		//This should be done in your php.ini, but this is how to do it if you don't have access to that
+		date_default_timezone_set('America/New_York');
+	}
 
-    public function getHeader() {
-        return $this->_header;
-    }
 
-    public function setTo($recipientEmail) {
-        $this->_to = $recipientEmail;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getSetFrom() {
+		return $this->_setFrom;
+	}
 
-    public function setSubjectLine($subjectLine) {
-        $this->_subjectLine = $subjectLine;
-    }
+	/**
+	 * @param mixed $setFrom
+	 */
+	public function setSetFrom( $setFrom ): void {
+		$this->_setFrom = $setFrom;
+	}
 
-    public function setContent($contentString) {
-        $this->_content = "<html><body>" . $contentString . "<p>" . _("This is an automatically generated email. Please do not respond.") . "</p><strong>" . _("Email sent: ") . date("l F j, Y, g:i a") . "</strong></body></html>";
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getAddReplyTo() {
+		return $this->_addReplyTo;
+	}
 
-    public function setFrom($from) {
-        $this->_fromString = "Library_No_Reply <" . $from . ">";
-        $this->_header = "Return-Path: $from\r\n";
-        $this->_header .= "From:  $this->_from\r\n";
-        $this->_header .= "Content-Type: text/html; charset=iso-8859-1;\n\n\r\n";
-    }
+	/**
+	 * @param mixed $addReplyTo
+	 */
+	public function setAddReplyTo( $addReplyTo ): void {
+		$this->_addReplyTo = $addReplyTo;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getAddAddress() {
+		return $this->_addAddress;
+	}
+
+	/**
+	 * @param mixed $addAddress
+	 */
+	public function setAddAddress( $addAddress ): void {
+		$this->_addAddress = $addAddress;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSubject() {
+		return $this->_Subject;
+	}
+
+	/**
+	 * @param mixed $Subject
+	 */
+	public function setSubject( $Subject ): void {
+		$this->_Subject = $Subject;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getMsgHTML() {
+		return $this->_msgHTML;
+	}
+
+	/**
+	 * @param mixed $msgHTML
+	 */
+	public function setMsgHTML( $msgHTML ): void {
+		$this->_msgHTML = $msgHTML;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getAltBody() {
+		return $this->_AltBody;
+	}
+
+	/**
+	 * @param mixed $AltBody
+	 */
+	public function setAltBody( $AltBody ): void {
+		$this->_AltBody = $AltBody;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getAddAttachment() {
+		return $this->_addAttachment;
+	}
+
+	/**
+	 * @param mixed $addAttachment
+	 */
+	public function setAddAttachment( $addAttachment ): void {
+		$this->_addAttachment = $addAttachment;
+	}
+
+
+//    private $_to;
+//    private $_from;
+//    private $_fromString;
+//    private $_subjectLine;
+//    private $_content;
+//    private $_header;
+//
+//    public function __construct($params=NULL) {
+//        if (is_array($params)) {
+//            if (isset($params['to'])) {
+//                $this->_to = $params['to'];
+//            }
+//            if (isset($params['subjectLine'])) {
+//                $this->_subjectLine = $params['subjectLine'];
+//            }
+//            if (isset($params['content'])) {
+//                $this->_content = $params['content'];
+//            }
+//            if (isset($params['from'])) {
+//                $this->_from = $params['from'];
+//                $this->_fromString = _("Library_No_Reply") . "<" . $this->_from . ">";
+//                $this->_header = "Return-Path: " . $this->_from . "\n";
+//                $this->_header .= "From:  " . $this->_from . "\n";
+//                $this->_header .= "Content-Type: text/html; charset=iso-8859-1;\n";
+//            }
+//        }
+//    }
+//
+//    public function getTo() {
+//        return $this->_to;
+//    }
+//
+//    public function getSubjectLine() {
+//        return $this->_subjectLine;
+//    }
+//
+//    public function getContent() {
+//        return $this->_content;
+//    }
+//
+//    public function getFrom() {
+//        return $this->_fromString;
+//    }
+//
+//    public function getHeader() {
+//        return $this->_header;
+//    }
+//
+//    public function setTo($recipientEmail) {
+//        $this->_to = $recipientEmail;
+//    }
+//
+//    public function setSubjectLine($subjectLine) {
+//        $this->_subjectLine = $subjectLine;
+//    }
+//
+//    public function setContent($contentString) {
+//        $this->_content = "<html><body>" . $contentString . "<p>" . _("This is an automatically generated email. Please do not respond.") . "</p><strong>" . _("Email sent: ") . date("l F j, Y, g:i a") . "</strong></body></html>";
+//    }
+//
+//    public function setFrom($from) {
+//        $this->_fromString = "Library_No_Reply <" . $from . ">";
+//        $this->_header = "Return-Path: $from\r\n";
+//        $this->_header .= "From:  $this->_from\r\n";
+//        $this->_header .= "Content-Type: text/html; charset=iso-8859-1;\n\n\r\n";
+//    }
 
 }
 ?>
--- lib/SubjectsPlus/Control/Mailer.php
+++ lib/SubjectsPlus/Control/Mailer.php
@@ -1,22 +1,203 @@
 <?php
 namespace SubjectsPlus\Control;
 
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Mailer {
+	
+	private $_mailer;
 
-  public function __construct() {
-  	global $email_server;
-  	global $administrator_email;
+	private $_isSmtp;
+	private $_SMTPDebug;
+	private $_Host;
+	private $_Port;
+	private $_SMTPAuth;
+	private $_Username;
+	private $_Password;
 
-  	ini_set("SMTP", $email_server);
-  	ini_set("sendmail_from", $administrator_email);
-  }
+	public function __construct() {
+		global $email_server;
+		global $administrator_email;
 
-  public function send(MailMessage $m) {
-    if (mail($m->getTo(), $m->getSubjectLine(), $m->getContent(), $m->getHeader())) {
-      return true;
-    }
-  }
+		ini_set( "SMTP", $email_server );
+		ini_set( "sendmail_from", $administrator_email );
+
+		//SMTP needs accurate times, and the PHP time zone MUST be set
+		//This should be done in your php.ini, but this is how to do it if you don't have access to that
+		date_default_timezone_set( 'America/New_York' );
+
+		//Create a new PHPMailer instance
+		$this->_mailer = new PHPMailer;
+		$this->configureMailer();
+
+	}
+	
+	
+	protected function configureMailer() {
+
+		//Tell PHPMailer to use SMTP
+		$this->_mailer->isSMTP();
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$this->_mailer->SMTPDebug = 2;
+	
+		//Set the hostname of the mail server
+		$this->_mailer->Host = 'smtp.cgcent.miami.edu';
+
+		//Set the SMTP port number - likely to be 25, 465 or 587
+		$this->_mailer->Port = 25;
+
+		//Whether to use SMTP authentication
+		$this->_mailer->SMTPAuth = false;
+
+		//Username to use for SMTP authentication
+		//$this->_mailer->Username = 'yourname@example.com';
+
+		//Password to use for SMTP authentication
+		//$this->_mailer->Password = 'yourpassword';
+
+		//Set who the message is to be sent from
+		$this->_mailer->setFrom( 'cgb37@miami.edu', 'Charles Brown-Roberts' );
+
+		//Set an alternative reply-to address
+		$this->_mailer->addReplyTo( 'cgb37@miami.edu', 'Charles Brown-Roberts' );
+
+		//Set who the message is to be sent to
+		$this->_mailer->addAddress( 'charlesbrownroberts@miami.edu', 'Charles Brown-Roberts' );
+
+		//Set the subject line
+		$this->_mailer->Subject = 'PHPMailer SMTP test';
+
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		$this->_mailer->msgHTML( 'message content' );
+
+		//Replace the plain text body with one created manually
+		$this->_mailer->AltBody = 'This is a plain-text message body';
+
+		//Attach an image file
+		//$this->_mailer->addAttachment('images/phpmailer_mini.png');
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getisSmtp() {
+		return $this->_isSmtp;
+	}
+
+	/**
+	 * @param mixed $isSmtp
+	 */
+	public function setIsSmtp( $isSmtp ): void {
+		$this->_isSmtp = $isSmtp;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSMTPDebug() {
+		return $this->_SMTPDebug;
+	}
+
+	/**
+	 * @param mixed $SMTPDebug
+	 */
+	public function setSMTPDebug( $SMTPDebug ): void {
+		$this->_SMTPDebug = $SMTPDebug;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getHost() {
+		return $this->_Host;
+	}
+
+	/**
+	 * @param mixed $Host
+	 */
+	public function setHost( $Host ): void {
+		$this->_Host = $Host;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getPort() {
+		return $this->_Port;
+	}
+
+	/**
+	 * @param mixed $Port
+	 */
+	public function setPort( $Port ): void {
+		$this->_Port = $Port;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSMTPAuth() {
+		return $this->_SMTPAuth;
+	}
+
+	/**
+	 * @param mixed $SMTPAuth
+	 */
+	public function setSMTPAuth( $SMTPAuth ): void {
+		$this->_SMTPAuth = $SMTPAuth;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUsername() {
+		return $this->_Username;
+	}
+
+	/**
+	 * @param mixed $Username
+	 */
+	public function setUsername( $Username ): void {
+		$this->_Username = $Username;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getPassword() {
+		return $this->_Password;
+	}
+
+	/**
+	 * @param mixed $Password
+	 */
+	public function setPassword( $Password ): void {
+		$this->_Password = $Password;
+	}
+
+
+	public function send() {
+
+		//send the message, check for errors
+		if ( ! $this->_mailer->send() ) {
+			echo 'Mailer Error: ' . $this->_mailer->ErrorInfo;
+		} else {
+			echo 'Message sent!';
+		}
+
+
+	}
+//  public function send(MailMessage $m) {
+//    if (mail($m->getTo(), $m->getSubjectLine(), $m->getContent(), $m->getHeader())) {
+//      return true;
+//    }
+//  }
 
 }
+
 ?>
