c571fe2de8d4901033cc4e68775f7138185168ba
Charles <charlesbrownroberts@gmail.com>
2019-04-19 07:38:58 -0400

    testing mail globals

--- subjects/mailer.php
+++ subjects/mailer.php
@@ -33,13 +33,59 @@ global $email_port;
 global $email_smtp_auth;
 global $email_smtp_debug;
 
-$mailer = new Mailer();
-$mailer->setHost($email_host);
-$mailer->setPort($email_port);
-$mailer->setSMTPAuth($email_smtp_auth);
-$mailer->setSMTPDebug($email_smtp_debug);
-$mailer->configureMessage();
-$mailer->send();
+var_dump($email_host);
+var_dump($email_port);
+var_dump($email_smtp_auth);
+var_dump($email_smtp_debug);
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.cgcent.miami.edu';
+//Set the SMTP port number - likely to be 25, 465 or 587
+$mail->Port = 25;
+//We don't need to set this as it's the default value
+//$mail->SMTPAuth = false;
+//Set who the message is to be sent from
+$mail->setFrom('cgb37@miami.edu', 'Charles Roberts');
+//Set an alternative reply-to address
+$mail->addReplyTo('cgb37@miami.edu', 'Charles Roberts');
+//Set who the message is to be sent to
+$mail->addAddress('charlesbrownroberts@miami.edu', 'Charles Roberts');
+//Set the subject line
+$mail->Subject = 'PHPMailer SMTP without auth test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML('message contents');
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//send the message, check for errors
+if (!$mail->send()) {
+	echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+	echo "Message sent!";
+}
+
+
+//$mailer = new Mailer();
+////$mailer->setHost($email_host);
+//$mailer->setHost('smtp.cgcent.miami.edu');
+////$mailer->setPort($email_port);
+//$mailer->setPort(25);
+////$mailer->setSMTPAuth($email_smtp_auth);
+//$mailer->setSMTPAuth(false);
+////$mailer->setSMTPDebug($email_smtp_debug);
+//$mailer->setSMTPDebug(2);
+//$mailer->configureMessage();
+//$mailer->send();
 
 
 
