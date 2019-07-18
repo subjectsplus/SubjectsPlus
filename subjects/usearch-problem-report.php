5332f67b3dfe68eadb005bf10a59e624f8a27684
Charles <cgb37@miami.edu>
2019-07-18 11:13:22 -0400

    testing slack

--- subjects/usearch-problem-report.php
+++ subjects/usearch-problem-report.php
@@ -317,7 +317,7 @@ if ( isset($_POST['problem_report_form']) && $_SERVER['REQUEST_METHOD'] === 'POS
 		return $result;
 	}
 
-	slack($webHookUrl, $message, $channel, $icon);
+	slack($problem_report_slack_webhook_url, $msg, $problem_report_slack_channel, $problem_report_slack_emoji);
 
 	/**
 	 * send comment to slack channel talkback
