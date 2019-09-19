34d7e3d0df19f6e7f067132baca542b5b7c0261e
Charles <cgb37@miami.edu>
2019-09-19 11:22:35 -0400

    if email from is empty use admin email, but don't insert that email into comment db and email message

--- subjects/talkback.php
+++ subjects/talkback.php
@@ -359,7 +359,7 @@ if ( isset( $_POST['the_suggestion'] ) && $_SERVER['REQUEST_METHOD'] === 'POST' 
 	if ( isset( $_POST["email_suggestion"] ) ) {
 		$this_name = scrubData($_POST["email_suggestion"]);
 	} else {
-		$this_name = "Anonymous";
+		$this_name = $administrator_email;
 	}
 
 	if ( isset( $_POST["the_suggestion"] ) ) {
@@ -369,6 +369,11 @@ if ( isset( $_POST['the_suggestion'] ) && $_SERVER['REQUEST_METHOD'] === 'POST' 
 	}
 
 
+	if( $this_name == $administrator_email) {
+		$this_comment_from = "Anonymous";
+	} else {
+		$this_comment_from = $this_name;
+	}
 
 	/**
 	 * TalkbackComment object
@@ -376,7 +381,7 @@ if ( isset( $_POST['the_suggestion'] ) && $_SERVER['REQUEST_METHOD'] === 'POST' 
 	 */
 	$newComment = new TalkbackComment();
 	$newComment->setQuestion( $this_comment );
-	$newComment->setQFrom( $this_name );
+	$newComment->setQFrom( $this_comment_from );
 	$newComment->setDateSubmitted( $todaycomputer );
 	$newComment->setDisplay( 'No' );
 	$newComment->setTbtags( $branch_filter );
@@ -422,7 +427,7 @@ if ( isset( $_POST['the_suggestion'] ) && $_SERVER['REQUEST_METHOD'] === 'POST' 
 
 	$msg = _( "New Comment via Talkback" ) . PHP_EOL;
 	$msg .= "$this_comment" . PHP_EOL;
-	$msg .= _( "From: " ) . $this_name . PHP_EOL;
+	$msg .= _( "From: " ) . $this_comment_from . PHP_EOL;
 	$msg .= _( "Date submitted: " ) . date( 'D M j, Y, g:i a' ) . PHP_EOL;
 	$msg .= _( "Tags: " ) . $set_filter . PHP_EOL;
 
