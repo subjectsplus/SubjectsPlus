c4f666c8e755b0a51745327702cd0a423bbaec53
Ali Sattar <alirsattar@gmail.com>
2020-05-05 19:19:33 -0400

    Cleanup of WorldCat.php

--- lib/SubjectsPlus/Control/Pluslet/WorldCat.php
+++ lib/SubjectsPlus/Control/Pluslet/WorldCat.php
@@ -16,24 +16,22 @@ class Pluslet_WorldCat extends Pluslet {
   
     $this->_type = "WorldCat";
     $this->_pluslet_bonus_classes = "type-worldcat";
-
-    // print_r( array(
-    //   'var_dump($this)' => var_dump($this)
-    // ));
-    // die;
   }
 
   protected function onEditOutput() {
-    global $worldcat_search_url;
-
     $this->_body = "<p class=\"faq-alert\">" . _("Click 'Save' to view your search box.") . "</p>";   
   }
 
   protected function onViewOutput() {
     global $worldcat_search_url;
 
-    if ( isset($worldcat_search_url) ) {
-      if ( trim($worldcat_search_url) == '' && isset($GLOBALS['canedit']) ) {
+    // Check whether to show alert notification telling them to set up WorldCat URL
+    $worldcat_url_exists = isset($worldcat_search_url);
+    $url_empty_string = trim($worldcat_search_url) == '';
+    $admin_view = isset($GLOBALS['canedit']);
+
+    if ( $worldcat_url_exists ) {
+      if ( $url_empty_string && $admin_view ) {
         $this->flashMessage =
           "<div
             class=\"faq-alert\"
@@ -48,8 +46,7 @@ class Pluslet_WorldCat extends Pluslet {
       };
     };
 
-    $output = $this->loadHtml(__DIR__ . '/views/WorldCat.html');
-      
+    $output = $this->loadHtml(__DIR__ . '/views/WorldCat.html');      
     $this->_body = "$output";
   }
 
@@ -59,7 +56,6 @@ class Pluslet_WorldCat extends Pluslet {
 
   static function getMenuIcon() {
     $icon="<span class=\"icon-text worldcat-text\">" . _("WorldCat Search") . "</span>";
-
     return $icon;
   }
 }
