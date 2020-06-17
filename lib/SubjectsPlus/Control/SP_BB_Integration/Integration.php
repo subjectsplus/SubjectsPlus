8852a12a2bc1d0cf3f7ce4b54180c1944dcb0d2c
Abel <afacendacarrasco@miami.edu>
2020-06-17 17:06:11 -0400

    added get all custom codes list function

--- lib/SubjectsPlus/Control/SP_BB_Integration/Integration.php
+++ lib/SubjectsPlus/Control/SP_BB_Integration/Integration.php
@@ -6,6 +6,7 @@ use SubjectsPlus\Control\Querier;
 class Integration
 {
     private $db;
+    private $db_connection;
 
     /**
      * SP_BB_Integration constructor.
@@ -14,23 +15,30 @@ class Integration
     public function __construct(Querier $db)
     {
         $this->db = $db;
+        $this->db_connection = $this->db->getConnection();
         $this->createTableIfNotExist();
     }
 
     private function createTableIfNotExist()
     {
-        $connection = $this->db->getConnection();
-        $statement = $connection->prepare("CREATE TABLE IF NOT EXISTS sp_bb_courses_relation (
+        $statement = $this->db_connection->prepare("CREATE TABLE IF NOT EXISTS sp_bb_courses_relation (
 id INT AUTO_INCREMENT PRIMARY KEY,
 custom_code MEDIUMTEXT  NOT NULL,
 associated_course_codes MEDIUMTEXT NOT NULL,
+description MEDIUMTEXT NULL,
 is_edited BOOLEAN DEFAULT FALSE,
 last_edited_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-last_edited_by_id INT NOT NULL,
-FOREIGN KEY (last_edited_by_id) REFERENCES staff(staff_id)
+last_edited_by_id INT NULL,
+FOREIGN KEY (last_edited_by_id) REFERENCES staff(staff_id) ON DELETE SET NULL
 )");
         $statement->execute();
 
+    }
+
+    public function getAllCustomCodesList(){
+        $statement = $this->db_connection->prepare("SELECT custom_code from sp_bb_courses_relation");
+        $statement->execute();
+        return $statement->fetchAll();
     }
 
 }
