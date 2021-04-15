215ea0cf5d351d0d8f586cbb0ac47ff60bcca40f
Jane Sandberg <sandbej@linnbenton.edu>
2021-04-15 08:56:28 -0700

    Add pluslet service

---
+++ src/Service/PlusletService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+class PlusletService
+{
+    public function plusletClassName(string $type): string
+    {
+        if ($type == 'Pluslet')
+        {
+            return "\SubjectsPlus\Control\Pluslet";
+        }
+        else
+        {
+            return "\SubjectsPlus\Control\Pluslet\\" . $type;
+        }
+    }
+
+}
+
+
+?>
