3314608b2aff7b432e4686197eb62a18f6360e97
pvillanueva <p.villanueva@miami.edu>
2021-04-19 16:10:51 -0400

    Update mega menu to include Library Exhibitions

--- subjects/includes/jekyll-site-header.php
+++ subjects/includes/jekyll-site-header.php
@@ -956,6 +956,7 @@ if (is_null($header_type)
 												</a></li>
 
 											<li><a class="dropdown-item no-decoration" href="https://www.library.miami.edu/about/events.html">Library Events</a></li>
+											<li><a class="dropdown-item no-decoration" href="https://www.library.miami.edu/about/library-exhibitions.html">Library Exhibitions</a></li>
 
 											<li><a class="dropdown-item no-decoration" href="https://news.miami.edu/libraries/index.html">Library News</a></li>
 
