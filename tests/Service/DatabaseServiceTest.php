818d29eb526b1f9fad1720c4beaf08aed58a0d24
Jane Sandberg <sandbej@linnbenton.edu>
2021-04-22 15:50:25 -0700

    Move new databases logic to a service

---
+++ tests/Service/DatabaseServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\DatabaseService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class DatabaseServiceTest extends KernelTestCase
+{
+    public function testDatabaseUrl()
+    {
+        $proxy_url = 'http://proxy.university.edu/login?url=';
+        $service = new DatabaseService();
+        $proxy_needed = $service->databaseUrl('http://ebsco.com/database', 0, $proxy_url);
+        $expected = 'http://proxy.university.edu/login?url=http://ebsco.com/database';
+        $this->assertSame($expected, $proxy_needed);
+
+        $arxiv_url = 'http://arxiv.org';
+        $no_proxy_needed = $service->databaseUrl($arxiv_url, 1, $proxy_url);
+        $this->assertSame($arxiv_url, $no_proxy_needed);
+
+    }
+
+}
