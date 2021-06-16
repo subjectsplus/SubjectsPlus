7bb8fe6422072aa643da64454ecee68cd327a8a5
Charles <cgb37@miami.edu>
2021-06-16 11:28:39 -0400

    ran composer install

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+];
