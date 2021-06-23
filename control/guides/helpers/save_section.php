582d0a5851982354294e897716d5d60438783707
Renel Desir <rxd702@miami.edu>
2021-06-23 17:07:15 -0400

    fixed includes control/guides/helpers/save_section.php

--- control/guides/helpers/save_section.php
+++ control/guides/helpers/save_section.php
@@ -13,9 +13,9 @@ header( "Cache-Control: no-store, no-cache, must-revalidate" );
 header( "Cache-Control: post-check=0, pre-check=0", false );
 header( "Pragma: no-cache" );
 
-require_once(__DIR__ . "/../../includes/autoloader.php" );
-require_once(__DIR__ . "/../../includes/config.php" );
-require_once(__DIR__ . "/../../includes/functions.php");
+require_once( "../../includes/autoloader.php" );
+require_once( "../../includes/config.php" );
+require_once("../../includes/functions.php");
 
 use SubjectsPlus\Control\Querier;
 use SubjectsPlus\Control\Guide\SectionService;
