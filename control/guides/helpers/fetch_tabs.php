b155694588f01cd2c4416677458167606a1ad556
Renel Desir <rxd702@miami.edu>
2021-06-24 15:20:22 -0400

    fixed requires control/guides/helpers/fetch_tabs.php

--- control/guides/helpers/fetch_tabs.php
+++ control/guides/helpers/fetch_tabs.php
@@ -6,8 +6,8 @@
  * Time: 12:34 PM
  */
 
-require_once("../../includes/autoloader.php");
-require_once("../../includes/config.php");
+require_once(__DIR__ . "/../../includes/autoloader.php");
+require_once(__DIR__ . "/../../includes/config.php");
 
 use SubjectsPlus\Control\Querier;
 use SubjectsPlus\Control\Guide\TabData;
