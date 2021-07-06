c15b6e14f92da6d06bbd52d9ebfaf4dcff270466
Renel Desir <rxd702@miami.edu>
2021-07-06 15:01:39 -0400

    fixed includes for subjects/talkback.php

--- subjects/talkback.php
+++ subjects/talkback.php
@@ -10,9 +10,9 @@ use SubjectsPlus\Control\Template;
 use SubjectsPlus\Control\ReCaptchaService;
 
 
-include( "../control/includes/config.php" );
-include( "../control/includes/functions.php" );
-include( "../control/includes/autoloader.php" );
+include_once(__DIR__ . "/../control/includes/config.php" );
+include_once(__DIR__ . "/../control/includes/functions.php" );
+include_once(__DIR__ . "/../control/includes/autoloader.php" );
 
 /**
  *
