de1588c6c7862f8a653dbafa0fcd4e9508f8208f
Renel Desir <rxd702@miami.edu>
2021-07-06 14:56:34 -0400

    fixed includes for subjects/index.php

--- subjects/index.php
+++ subjects/index.php
@@ -17,9 +17,9 @@ use SubjectsPlus\Control\CompleteMe;
 use SubjectsPlus\Control\Querier;
 use SubjectsPlus\Control\Guide\GuideList;
 
-include("../control/includes/config.php");
-include("../control/includes/functions.php");
-include("../control/includes/autoloader.php");
+include_once(__DIR__ . "/../control/includes/config.php");
+include_once(__DIR__ . "/../control/includes/functions.php");
+include_once(__DIR__ . "/../control/includes/autoloader.php");
 
 $this_fname = "index.php";
 $that_fname = theme_file($this_fname, $subjects_theme);
