a0e641afd2ea919716bd8974f224dc2641d57fbc
Renel Desir <rxd702@miami.edu>
2021-07-08 11:04:25 -0400

    fixed directory references for collection.php

--- subjects/collection.php
+++ subjects/collection.php
@@ -13,7 +13,7 @@ include_once(__DIR__ . "/../control/includes/config.php");
 include_once(__DIR__ . "/../control/includes/functions.php");
 include_once(__DIR__ . "/../control/includes/autoloader.php");
 
-$this_fname = "collection.php";
+$this_fname = __DIR__ . "/collection.php";
 $that_fname = theme_file($this_fname, $subjects_theme);
 if ( $this_fname != $that_fname ) { include_once($that_fname); exit; }
    
@@ -122,7 +122,7 @@ $searchbox = '
 
 // Add header now 
 
-include_once(theme_file("includes/header.php", $subjects_theme));
+include_once(theme_file(__DIR__ . "/includes/header.php", $subjects_theme));
 
 // put together our main result display
 
@@ -194,7 +194,7 @@ $layout = makePluslet("", $guide_results, "","",FALSE);
 // Load footer file
 ///////////////////////////
 
-include_once(theme_file("includes/footer.php", $subjects_theme));
+include_once(theme_file(__DIR__ . "/includes/footer.php", $subjects_theme));
 
 ?>
 
