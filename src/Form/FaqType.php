2d345d2650d156aae2e8d493243b088b78d48981
Renel Desir <rxd702@miami.edu>
2021-07-27 11:35:35 -0400

    added ckeditor 4; fixed keywords null error when updating data

--- src/Form/FaqType.php
+++ src/Form/FaqType.php
@@ -3,9 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Faq;
+use App\Entity\Subject;
+use App\Entity\FaqSubject;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FaqType extends AbstractType
 {
@@ -13,9 +18,11 @@ class FaqType extends AbstractType
     {
         $builder
             ->add('question')
-            ->add('answer')
-            ->add('keywords')
-        ;
+            ->add('answer', CKEditorType::class)
+            ->add('keywords', TextType::class, [
+                'required' => false,
+                'empty_data' => '',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
