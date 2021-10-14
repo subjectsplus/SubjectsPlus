4c36f5b2b08602b59459a85181ecbd3252e4a219
Renel Desir <rxd702@miami.edu>
2021-10-14 12:22:01 -0400

    removed vichbundle usage from media type form

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -6,18 +6,15 @@ use App\Entity\Media;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class MediaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('file', VichFileType::class, [
-            'required' => false,
-            'allow_delete' => false,
-            'asset_helper' => true,
-            'download_label' => false,
-            'label' => false,
+        $builder->add('file', FileType::class, [
+            'required' => true,
+            'label' => 'Select File',
         ]);
     }
 
