72725e5be657d52ad7ebdeafa09c3b9ea9c9fc04
Renel Desir <rxd702@miami.edu>
2021-10-19 12:26:43 -0400

    add filename size constraint, function for subdir from mimetype

--- src/Service/FileNamerService.php
+++ src/Service/FileNamerService.php
@@ -3,13 +3,19 @@
 namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use App\Entity\Media;
 
 class FileNamerService
 {
+    private const MAX_EXTENSION_CHAR_COUNT = 10;
+
     public function directoryName(UploadedFile $file): string
     {
         $mimeType = $file->getMimeType();
+        return $this->getSubDirectoryFromMimeType($mimeType);
+    }
+
+    public function getSubDirectoryFromMimeType(string $mimeType): string
+    {
         $subDir = "";
         
         if (strpos($mimeType, "image/") !== false) {
@@ -31,10 +37,23 @@ class FileNamerService
         $origFileName = pathinfo($origBaseName, PATHINFO_FILENAME);
         $extension = pathinfo($origBaseName, PATHINFO_EXTENSION);
         $uuid = str_replace('.', '', uniqid('-', true));
+
+        // if extension is too long, shorten it
+        if (strlen($extension) > self::MAX_EXTENSION_CHAR_COUNT) {
+            $extension = substr($extension, 0, self::MAX_EXTENSION_CHAR_COUNT);
+        }
+        
+        // max file name character count, accounting for extension, uuid, and dot character
+        $maxFileName = 255 - strlen($extension) - strlen($uuid) - 1;
+        
+        // if file name is longer than the space left, shorten it
+        if (strlen($origFileName) > $maxFileName) {
+            $origFileName = substr($origFileName, 0, $maxFileName);
+        }
+
         $uniqExtension = sprintf('%s.%s', $uuid, $extension);
         $fileName = sprintf('%s%s', $origFileName, $uniqExtension);
 
-        // todo: check if within 255 characters
         return $fileName;
     }
 }
