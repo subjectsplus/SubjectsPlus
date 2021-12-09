6739090ed377f40a9182263da07b93a5d1ed382d
pvillanueva <p.villanueva@miami.edu>
2021-12-08 19:50:00 -0500

    update scss structure

--- assets/backend/backend-main.js
+++ assets/backend/backend-main.js
@@ -5,8 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+// any CSS you import will output into a single css file (backend-main.css in this case)
 import '../styles/backend/global.scss';
 
-// start the Stimulus application
-//import './bootstrap';
+// Bootstrap JS plugins
+require('bootstrap');
