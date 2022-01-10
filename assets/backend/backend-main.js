70130063a9eff3bd6177be467733819625f73d89
pvillanueva <p.villanueva@miami.edu>
2022-01-10 16:22:05 -0500

    Set up CSS structure with Bootstrap and partials

--- assets/backend/backend-main.js
+++ assets/backend/backend-main.js
@@ -8,14 +8,5 @@
 // any CSS you import will output into a single css file (backend-main.css in this case)
 import '../styles/backend/global.scss';
 
-// Bootstrap JS plugins
-//require('bootstrap');
-
+// Bootstrap JS
 window.bootstrap = require("bootstrap");
-
-//window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
-
-//import * as bootstrap from 'bootstrap';
-//window.bootstrap = bootstrap;
-
-//const bootstrap = window.bootstrap = require('bootstrap');
