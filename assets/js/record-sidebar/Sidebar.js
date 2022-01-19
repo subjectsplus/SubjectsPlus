4a8081149141228d7a399b47edb6526431d8ede4
Renel Desir <rxd702@miami.edu>
2022-01-19 09:33:47 -0500

    implement infinite scroll, replace jquery with fetch

--- assets/js/record-sidebar/Sidebar.js
+++ assets/js/record-sidebar/Sidebar.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Token from './Token.js';
 import SearchBar from './SearchBar.js';
 
 export default class Sidebar extends Component {
-    apiLink = '/api/titles?search={search_term}'
+    apiLink = '/api/titles';
 
     constructor(props) {
         super(props);
 
         this.state = {
             records: [],
+            page: 1,
+            hasNextPage: false,
             previousInput: null,
             inputEmpty: true,
-            azlist: true
+            azlist: true,
+            isErrored: false
         };
 
         this.onSearchInput = this.onSearchInput.bind(this);
         this.onAZListCheckBoxInput = this.onAZListCheckBoxInput.bind(this);
+        this.loadNextPage = this.loadNextPage.bind(this);
     }
 
     onSearchInput(evt) {
         var userInput = evt.target.value;
         if (typeof userInput === 'string' && userInput.length >= 3) {
-            this.getRecords(userInput, this.state.azlist);
+            if (userInput === this.state.previousInput) {
+                this.getRecords(userInput, this.state.page, this.state.azlist);
+            } else {
+                this.getRecords(userInput, 1, this.state.azlist);
+            }
         } else {
-            this.setState({inputEmpty: true});
+            this.setState({inputEmpty: true, page: 1});
         }
     }
 
@@ -34,30 +43,65 @@ export default class Sidebar extends Component {
         this.setState({azlist: checked});
         
         if (!this.state.inputEmpty && this.state.previousInput) {
-            this.getRecords(this.state.previousInput, checked);
+            this.getRecords(this.state.previousInput, 1, checked);
         }
     }
 
-    getRecords(search_term, use_az_list=true) {
-        var resLink = this.apiLink.replace('{search_term}', search_term);
-        if (use_az_list) {
-            resLink += '&location.eresDisplay=Y';
-        }
+    getRecords(search_term, page=1, use_az_list=true, append=false) {
+        // formulate the results api link
+        var resLink = this.apiLink + '?' + new URLSearchParams({
+            search: search_term,
+            'location.eresDisplay': (use_az_list ? 'Y' : 'N'),
+            page: page
+        });
 
-        $.getJSON(resLink).then((results) => {
+        // only append results from subsequent pages
+        if (page === 1) append = false;
+
+        fetch(resLink).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            this.setState({
+                isErrored: true
+            });
+        })
+        .then(results => {
+            console.log(results['hydra:member']);
+            console.log(results['hydra:view']);
+            console.log(results['hydra:view']['hydra:next']);
+            console.log((results['hydra:view']['hydra:next'] != null));
+
             this.setState({
                 inputEmpty: false, 
                 previousInput: search_term, 
-                records: results
+                records: (append ? this.state.records.concat(results['hydra:member']) : results['hydra:member']),
+                page: page,
+                hasNextPage: (results['hydra:view']['hydra:next'] != null),
+                isErrored: false
+            });
+        }
+        )
+        .catch(err => {
+            console.error(err);
+            this.setState({
+                isErrored: true
             });
         });
+    }
+
+    loadNextPage() {
+        this.getRecords(this.state.previousInput, this.state.page + 1, this.state.azlist, true);
     }
 
     render() {
         var resultsMessage = 'Please enter a search term. (Minimum 3 characters)';
         var recordTokens = [];
 
-        if (!this.state.inputEmpty) {
+        if (this.state.isErrored) resultsMessage = "Error: Failed to reach API endpoint.";
+
+        if (!this.state.inputEmpty && !this.state.isErrored) {
             recordTokens = this.state.records.map( (record, index) => (
                 <li key={record.titleId}>
                     <Token tokenClassName="record-token" recordId={record.titleId} recordTitle={record.title}
@@ -78,7 +122,19 @@ export default class Sidebar extends Component {
                      Limit to A-Z List
                 </label>
                 <ul id="records-list">
-                    {recordTokens.length > 0 ? recordTokens : resultsMessage}
+                    <InfiniteScroll
+                        dataLength={this.state.records.length} //This is important field to render the next data
+                        next={this.loadNextPage}
+                        hasMore={this.state.hasNextPage}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={
+                            <p style={{ textAlign: 'center' }}>
+                            <b>End of results.</b>
+                            </p>
+                        }
+                        scrollableTarget="records-list">
+                        {recordTokens.length > 0 ? recordTokens : resultsMessage}
+                    </InfiniteScroll>
                 </ul>
             </div>
         );
