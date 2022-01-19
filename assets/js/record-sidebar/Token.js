f3dddc6c79584027c82b9ecbf05b6d1d0f08384b
Renel Desir <rxd702@miami.edu>
2022-01-19 09:32:30 -0500

    use html entity decode before displaying record title

--- assets/js/record-sidebar/Token.js
+++ assets/js/record-sidebar/Token.js
@@ -6,13 +6,20 @@ export default class Token extends Component {
         super(props);
     }
 
+    htmlEntityDecode(str) {
+        if (typeof str !== 'string') return '';
+
+        var doc = new DOMParser().parseFromString(str, 'text/html');
+        return doc.body.textContent || '';
+    }
+
     render() {
         return (
             <div className={this.props.tokenClassName} draggable="true"
                 data-record-id={this.props.recordId} data-record-title={this.props.recordTitle}
                 data-record-description={this.props.recordDescription} 
                 data-record-location={this.props.recordLocation}>
-                    {this.props.recordTitle}
+                    {this.htmlEntityDecode(this.props.recordTitle)}
             </div>
         );
     }
