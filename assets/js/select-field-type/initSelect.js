3238c0e671afc31c7cb42619effc9ae0ef6e9cc5
Renel Desir <rxd702@miami.edu>
2022-02-08 12:01:26 -0500

    add classNamePrefix

--- assets/js/select-field-type/initSelect.js
+++ assets/js/select-field-type/initSelect.js
@@ -9,6 +9,6 @@ Array.from(selectElements).forEach( element => {
     let selectedOptions = Array.from(element.querySelectorAll('option:checked'));
     
     Utility.replaceNodeWithReactComponent(element,
-        <Select isMulti={true} id={element.id} name={element.getAttribute('name')} options={options} 
+        <Select isMulti={true} id={element.id} classNamePrefix="sp-react-select" name={element.getAttribute('name')} options={options} 
             defaultValue={selectedOptions} />);
 });
