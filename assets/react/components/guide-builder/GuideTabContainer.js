176fc497fb20a057aa704c5f93d41ee42057cb29
Renel Desir <rxd702@miami.edu>
2022-02-16 16:18:59 -0500

    add new tab functionality

--- assets/react/components/guide-builder/GuideTabContainer.js
+++ assets/react/components/guide-builder/GuideTabContainer.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
+import Button from 'react-bootstrap/Button'
 import Utility from '../../../js/Utility/Utility.js';
 import SectionContainer from './SectionContainer.js';
 
 export default class GuideTabContainer extends Component {
     apiLink = '/api/subjects/{subjectId}/tabs';
+    postLink = '/api/tabs';
 
     constructor(props) {
         super(props);
         
         this.state = {
             tabs: null,
+            lastTabIndex: 0,
+            activeKey: 0,
             isErrored: false
         };
+
+        this.onTabSelect = this.onTabSelect.bind(this);
     }
 
     componentDidMount() {
@@ -41,7 +47,8 @@ export default class GuideTabContainer extends Component {
         })
         .then(results => {
             this.setState({
-                tabs: results["hydra:member"],
+                tabs: results['hydra:member'],
+                lastTabIndex: results['hydra:member'].at(-1)['tabIndex'],
                 isErrored: false
             });
         }
@@ -54,21 +61,80 @@ export default class GuideTabContainer extends Component {
         });
     }
 
+    onTabSelect(eventKey) {
+        console.log(eventKey);
+        if (eventKey === 'new-tab') {
+            // Create new tab
+            this.newTab();
+        } else if (this.state.activeKey !== eventKey) {
+            this.setState({
+                activeKey: eventKey
+            });
+        }
+    }
+
+    newTab() {
+        let initialTabData = {
+            label: 'Untitled',
+            tabIndex: this.state.lastTabIndex + 1,
+            visibility: true,
+            subject: '/api/subjects/' + this.props.guideId
+        };
+
+        fetch(this.postLink, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(initialTabData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                tabs: [...this.state.tabs, data],
+                lastTabIndex: data.tabIndex,
+                activeKey: data.tabIndex
+            });
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            alert('Error: Failed to add new tab.');
+            console.error('Error:', error);
+        });
+    }
+
+    deleteTab(tabId) {
+
+    }
+
     render() {
         if (this.state.tabs) {
-            var guideTabs = this.state.tabs.map((results, index) => {
+            let guideTabs = this.state.tabs.map((results, index) => {
                 return (
                     <Tab key={results.tabId} eventKey={results.tabIndex} 
-                        title={Utility.htmlEntityDecode(results.label)}>
+                        title={
+                            <>
+                                {Utility.htmlEntityDecode(results.label)}{' '}
+                                <Button key={'-tab-settings-' + results.tabId} className="tab-settings-button"
+                                    variant="outline-primary" size="sm">
+                                    <i className="fas fa-cog"></i>
+                                </Button>
+                            </>
+                        }>
                         <SectionContainer tabId={results.tabId} />
                     </Tab>
                 );
             });
             console.log(guideTabs);
             return (
-                <Tabs defaultActiveKey="0" id="guide-tabs-container">
-                    {guideTabs}
-                </Tabs>
+                <div id="guide-tabs-container">
+                    <Tabs activeKey={this.state.activeKey} id="guide-tabs" onSelect={this.onTabSelect}>
+                        {guideTabs}
+                        <Tab key="new-tab" eventKey="new-tab" 
+                            title={<i className="fas fa-plus"></i>} 
+                        />
+                    </Tabs>
+                </div>
             );
         } else if (this.state.isErrored) {
             return (<p>Error: Failed to load tabs through API Endpoint!</p>);
