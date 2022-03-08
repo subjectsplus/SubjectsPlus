2c2258279a90776c051555b4bfb722df5cc79354
Charles <cgb37@miami.edu>
2022-03-08 12:35:12 -0500

    update login route
    change login route to /control/login

--- src/Controller/Backend/AuthController.php
+++ src/Controller/Backend/AuthController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class AuthController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/control/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
