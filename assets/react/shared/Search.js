54eecb8b35963c67c37ce7e7f3a40643322e9167
Renel Desir <rxd702@miami.edu>
2022-03-09 14:43:30 -0500

    add loading feedback to the user

--- assets/react/shared/Search.js
+++ assets/react/shared/Search.js
@@ -24,53 +24,58 @@ export default class Search extends Component {
     }
 
     onSearchInput(evt) {
-        var userInput = evt.target.value;
+        let userInput = evt.target.value;
         if (typeof userInput === 'string' && userInput.length >= 3) {
             if (userInput !== this.state.previousInput) {
                 this.getResults(userInput, 1);
             }
         } else {
-            this.setState({inputEmpty: true, page: 1});
+            this.setState({inputEmpty: true, previousInput: '', page: 1});
         }
     }
 
     getResults(search_term, page=1, append=false) {
         // formulate the results api link
-        var resLink = this.props.apiLink(search_term, page);
+        let resLink = this.props.apiLink(search_term, page);
 
         // only append results from subsequent pages
         if (page === 1) append = false;
 
         // fetch api results
-        fetch(resLink).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
+        this.setState({
+            loading: true, 
+            results: []
+            }, 
+            () => fetch(resLink).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
 
-            this.setState({
-                isErrored: true,
-                loading: false
-            });
-        })
-        .then(results => {
-            this.setState({
-                inputEmpty: false, 
-                previousInput: search_term, 
-                results: (append ? this.state.results.concat(results['hydra:member']) : results['hydra:member']),
-                page: page,
-                hasNextPage: (results['hydra:view']['hydra:next'] != null),
-                isErrored: false,
-                loading: false
-            });
-        }
-        )
-        .catch(err => {
-            console.error(err);
-            this.setState({
-                isErrored: true,
-                loading: false
-            });
-        });
+                this.setState({
+                    isErrored: true,
+                    loading: false
+                });
+            })
+            .then(results => {
+                this.setState({
+                    inputEmpty: false, 
+                    previousInput: search_term, 
+                    results: (append ? this.state.results.concat(results['hydra:member']) : results['hydra:member']),
+                    page: page,
+                    hasNextPage: (results['hydra:view']['hydra:next'] != null),
+                    isErrored: false,
+                    loading: false
+                });
+            }
+            )
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    isErrored: true,
+                    loading: false
+                });
+            })
+        );
     }
 
     refresh() {
@@ -88,15 +93,9 @@ export default class Search extends Component {
 
     render() {
         // TODO: Translations for text below
-        var resultsMessage = (
-            <p className="fs-sm fst-italic">Please enter a search term (minimum 3 characters).</p>
-        );
-        var resultTokens = [];
-        var  bottomElement = null;
-
-        if (this.state.isErrored) resultsMessage = (
-            <p className="fs-sm fst-italic">Error: Failed to reach API endpoint.</p>
-        );
+        let resultsMessage = '';
+        let resultTokens = [];
+        let bottomElement = null;
         
         // Turn the current results into token components
         if (!this.state.inputEmpty && !this.state.isErrored) {
@@ -109,7 +108,19 @@ export default class Search extends Component {
                         </li>
                     )}
             });
-            if (resultTokens.length === 0) resultsMessage = (
+        }
+
+        // Determine results message if applicable
+        if (this.state.isErrored) {
+            resultsMessage = (
+                <p className="fs-sm fst-italic">Error: Failed to reach API endpoint.</p>
+            );
+        } else if (this.state.inputEmpty) {
+            resultsMessage = (
+                <p className="fs-sm fst-italic">Please enter a search term (minimum 3 characters).</p>
+            );
+        } else if (resultTokens.length === 0 && !this.state.loading) {
+            resultsMessage = (
                 <p className="fs-sm fst-italic">No results found.</p>
             );
         }
