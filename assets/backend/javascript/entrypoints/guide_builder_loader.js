623ecbc04d789c91c3901b45d7dd2696e079de64
Renel Desir <rxd702@miami.edu>
2022-03-15 11:54:23 -0400

    add react query to guide builder

--- assets/backend/javascript/entrypoints/guide_builder_loader.js
+++ assets/backend/javascript/entrypoints/guide_builder_loader.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GuideBuilder from '../../../react/components/guide-builder/GuideBuilder.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 // TODO: Revisit this method of gathering the guide id
 // possibly without relying on the client window location
-let path = window.location.pathname;
-let guideId = path.split("/").pop();
+const path = window.location.pathname;
+const subjectId = path.split("/").pop();
 
-ReactDOM.render(<GuideBuilder guideId={guideId} />, 
-    document.getElementById('guide-builder-container'));
+// Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+    <QueryClientProvider client={queryClient}>
+        <GuideBuilder subjectId={subjectId} />
+        <ReactQueryDevtools initialIsOpen={false} position="top-right" />
+    </QueryClientProvider>, 
+    document.getElementById('guide-builder-container')
+);
