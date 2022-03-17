8dbaeedd098c4c437933582f5e41517bf0ab77be
Renel Desir <rxd702@miami.edu>
2022-03-17 13:21:56 -0400

    change sectioncontainer to functional component

--- assets/react/components/guide-builder/SectionContainer.js
+++ assets/react/components/guide-builder/SectionContainer.js
@@ -1,70 +1,28 @@
-import React, { Component } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
-import Section from './Section.js';
+import React, { useMemo } from 'react';
+import { useFetchSections } from '#api/guide/SectionAPI';
+import Section from './Section';
 
-export default class SectionContainer extends Component {
-    apiLink = '/api/tabs/{tabId}/sections';
+function SectionContainer({ tabId }) {
+    const {isLoading, isError, data, error} = useFetchSections(tabId);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            sections: null,
-            isErrored: false
-        };
-    }
-
-    componentDidMount() {
-        this.getSections();
-    }
-
-    getAPILink() {
-        return this.apiLink.replace('{tabId}', 
-            this.props.tabId);
-    }
-
-    getSections() {
-        // formulate the results api link for guide
-        var resLink = this.getAPILink();
-
-        // fetch api results
-        fetch(resLink).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-
-            this.setState({
-                isErrored: true
-            });
-        })
-        .then(results => {
-            this.setState({
-                sections: results["hydra:member"],
-                isErrored: false
-            });
-        }
-        )
-        .catch(err => {
-            console.error(err);
-            this.setState({
-                isErrored: true
-            });
-        });
-    }
-
-    render() {
-        if (this.state.sections) {
-            let guideSections = this.state.sections.map((result, index) => 
-                <Section key={result.sectionId} sectionId={result.sectionId} />)
+    const containerContent = useMemo(() => {
+        if (isError) {
+            console.error(error);
+            return (<p>Error: Failed to load sections through API Endpoint!</p>);
+        } else if (isLoading) {
+            return (<p>Loading Sections...</p>);
+        } else {
+            const guideSections = data.map(section => 
+                <Section key={section.sectionId} sectionId={section.sectionId} layout={section.layout} />);
             return (
                 <div className="section-container">
                     {guideSections}
                 </div> 
             );
-        } else if (this.state.isErrored) {
-            return (<p>Error: Failed to load sections through API Endpoint!</p>);
-        } else {
-            return (<p>Loading Sections...</p>);
         }
-    }
+    }, [data]);
+
+    return containerContent;
 }
+
+export default SectionContainer;
