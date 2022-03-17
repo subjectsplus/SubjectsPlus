f96bbac31cc9d6f8a8b2d74d30e854e7d854567d
Renel Desir <rxd702@miami.edu>
2022-03-17 16:11:51 -0400

    add dragdropcontext for sectioncontainer

--- assets/react/components/guide-builder/SectionContainer.js
+++ assets/react/components/guide-builder/SectionContainer.js
@@ -1,9 +1,19 @@
 import React, { useMemo } from 'react';
 import { useFetchSections } from '#api/guide/SectionAPI';
 import Section from './Section';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function SectionContainer({ tabId }) {
     const {isLoading, isError, data, error} = useFetchSections(tabId);
+
+    const handleOnDragEnd = (result) => {
+        console.log(result);
+        if (result.type === 'section') {
+            console.log('Section onDragEnd Handler');
+        } else if (result.type === 'pluslet') {
+            console.log('Pluslet onDragEnd Handler');
+        }
+    }
 
     const containerContent = useMemo(() => {
         if (isError) {
@@ -15,9 +25,16 @@ function SectionContainer({ tabId }) {
             const guideSections = data.map(section => 
                 <Section key={section.sectionId} sectionId={section.sectionId} layout={section.layout} />);
             return (
-                <div className="section-container">
-                    {guideSections}
-                </div> 
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable type="section" style={{ transform: "none" }} droppableId="guide-section-container" direction="vertical">
+                        {(provided) => (
+                            <div className="section-container" {...provided.droppableProps} ref={provided.innerRef}>
+                                {guideSections}
+                                {provided.placeholder}
+                            </div> 
+                        )}
+                    </Droppable>
+                </DragDropContext>
             );
         }
     }, [data]);
