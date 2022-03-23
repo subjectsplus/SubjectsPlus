398c0409145d0c54e0510110457a5d5cffc93366
Renel Desir <rxd702@miami.edu>
2022-03-23 15:17:04 -0400

    add drag handle and styling, pluslet row/col wip

--- assets/react/components/guide-builder/Section.js
+++ assets/react/components/guide-builder/Section.js
@@ -1,10 +1,60 @@
 import React, { useMemo } from 'react';
 import Pluslet from './Pluslet';
 import { useFetchPluslets } from '#api/guide/PlusletAPI';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function Section({ sectionId, layout }) {
+function Section({ sectionId, layout, sectionIndex }) {
     const {isLoading, isError, data, error} = useFetchPluslets(sectionId);
+
+    const padding = 16;
+
+    const getSectionDraggableStyle = (isDragging, draggableStyle) => ({
+        padding: `${padding * 2}px`,
+        position: 'relative',
+        marginBottom: `${padding}px`,
+        background: isDragging ? "lightgreen" : "grey",
+        ...draggableStyle
+      });
+
+    const getSectionDroppableStyle = (isDraggingOver) => ({
+        background: isDraggingOver ? "lightblue" : "lightgrey",
+        padding: padding,
+        width: 200
+    });
+
+    const sectionAnchorStyle = {
+        position: 'absolute',
+        display: 'block',
+        border: '1px solid #000',
+        width: '50px',
+        height: '20px',
+        lineHeight: '20px',
+        right: 0,
+        top: 0,
+        marginTop: '-10px',
+        zindex: 1
+    }
+
+    const generateContainer = (pluslets) => {
+        const splitLayout = layout.split('-');
+        
+        const rows = splitLayout.map(size => {
+
+            if (Number(size) !== 0) {
+                return (
+                    <Row>
+                        <Col lg={Number(size)}>
+
+                        </Col>
+                    </Row>
+                )
+            }
+        });
+
+    }
 
     const sectionContent = useMemo(() => {
         if (isLoading) {
@@ -13,28 +63,40 @@ function Section({ sectionId, layout }) {
             console.error(error);
             return (<p>Error: Failed to load sections through API Endpoint!</p>);
         } else {
-            const pluslets = data.map((pluslet, index) => 
+            const pluslets = data.map((pluslet, index) =>
                 <Pluslet key={pluslet.plusletId} plusletId={pluslet.plusletId} plusletRow={index} />
             );
 
             return (
-                <Droppable type="pluslet" key={sectionId.toString()} style={{ transform: "none" }} 
-                    droppableId={sectionId.toString()} direction="vertical">
-                    {(provided) => (
-                        <div className="guide-section" data-layout={layout} {...provided.droppableProps} ref={provided.innerRef}
-                            style={{
-                                border: '2px solid black',
-                                height: '500px',
-                                width: '750px',
-                            }}>
-                            {pluslets}
-                            {provided.placeholder}
+                <Draggable type="section" draggableId={'section-' + sectionIndex} index={sectionIndex}>
+                    {(provided, snapshot) => (
+                        <div ref={provided.innerRef} {...provided.draggableProps}
+                            style={getSectionDraggableStyle(snapshot.isDragging, provided.draggableProps.style)}>
+                            <span style={sectionAnchorStyle}>
+                                <span className="drag-handle" {...provided.dragHandleProps}>
+                                    <i className="fas fa-arrows-alt"></i>
+                                </span>
+                                <span className="select-more">
+                                    <i className="fas fa-ellipsis-v"></i>
+                                </span>
+                            </span>
+                            <Droppable type="pluslet" key={sectionId.toString()} style={{ transform: "none" }} 
+                                droppableId={sectionId.toString()} direction="vertical">
+                                {(provided, snapshot) => (
+                                    <div className="guide-section" data-layout={layout} {...provided.droppableProps} 
+                                        ref={provided.innerRef}
+                                        style={getSectionDroppableStyle(snapshot.isDraggingOver)}>
+                                        {pluslets.length > 0 ? pluslets : 'Add a pluslet'}
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
                         </div>
                     )}
-                </Droppable>
+                </Draggable>
             );
         }
-    }, [data]);
+    }, [data, isLoading, isError]);
 
     return sectionContent;
 }
