445cb75604364fa581760bdb564a3dcd65980680
Renel Desir <rxd702@miami.edu>
2022-03-31 14:54:37 -0400

    reorder pluslet wip

--- assets/react/api/guide/PlusletAPI.js
+++ assets/react/api/guide/PlusletAPI.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 export function useFetchPluslets(sectionId) {
     if (sectionId === undefined) throw new Error('"sectionId" argument is required to call useFetchPluslets.');
@@ -16,8 +16,87 @@ export function useFetchPluslet(plusletId) {
     return useQuery(['pluslet', plusletId], () => fetchPluslet(plusletId));
 }
 
-async function fetchPluslets(sectionId) {
-    const data = await fetch(`/api/sections/${sectionId}/pluslets`);
+export function useReorderPluslet() {
+    const queryClient = useQueryClient();
+    return useMutation(reorderPluslet, {
+        onMutate: async plusletData => {
+            await queryClient.cancelQueries(['pluslets', plusletData.sectionId]);
+            
+            const sourcePlusletsData = queryClient.getQueryData(['pluslets', plusletData.sectionId]);
+            const sourcePluslets = [...sourcePlusletsData['hydra:member']];
+            const sourceColumn = plusletData.sourceColumn;
+            const sourceIndex = plusletData.sourceIndex;
+            const destinationColumn = plusletData.destinationColumn;
+            const destinationIndex = plusletData.destinationIndex;
+
+            // Produce optimistic result
+            if (sourceColumn === destinationColumn) {
+                const columnPluslets = sourcePluslets.filter(pluslet => 
+                    pluslet.pcolumn === sourceColumn);
+
+                // Move pluslet within the same column
+                const [reorderedItem] = columnPluslets.splice(sourceIndex, 1);
+                columnPluslets.splice(destinationIndex, 0, reorderedItem);
+
+                // Set the updated prow
+                columnPluslets.forEach((pluslet, index) => {
+                    pluslet.prow = index;
+                });
+
+                // Set optimistic result to query data
+                queryClient.setQueryData(['pluslets', plusletData.sectionId], {
+                    ...previousPlusletsData,
+                    'hydra:member': sourcePluslets,
+                });
+
+                return { sourcePlusletsData };
+            } else {
+                const sourceColumnPluslets = sourcePluslets.filter(pluslet => 
+                    pluslet.pcolumn === sourceColumn);                
+                
+                // Remove pluslet from source column
+                const [reorderedItem] = sourceColumnPluslets.splice(sourceIndex, 1);
+
+                // Set the updated prow for source column pluslets
+                sourceColumnPluslets.forEach((pluslet, index) => {
+                    pluslet.prow = index;
+                });
+
+                // Move pluslet to a different column
+                const destinationColumnPluslets = sourcePluslets.filter(pluslet => 
+                    pluslet.pcolumn === destinationColumn);
+
+                // Add to destination column and reorder destination column pluslets 
+                destinationColumnPluslets.splice(destinationIndex, 0, reorderedItem);
+                destinationColumnPluslets.forEach((pluslet, index) => {
+                    pluslet.prow = index;
+                });
+
+                // Set optimistic result to query data
+                queryClient.setQueryData(['pluslets', plusletData.sectionId], {
+                    ...previousPlusletsData,
+                    'hydra:member': sourcePluslets,
+                });
+
+                return { sourcePlusletsData };
+
+            }
+        },
+        onError: (error, plusletsData, context) => {
+            // Perform rollback of tab mutation
+            console.error(error);
+            queryClient.setQueryData(['pluslets', plusletsData.sectionId], context.sourcePlusletsData);
+        },
+        onSettled: () => {
+            // Refetch the tab data
+            queryClient.invalidateQueries(['pluslets', plusletsData.sectionId]);
+        }
+    });
+}
+
+async function fetchPluslets(sectionId, filters=null) {
+    const data = await fetch(`/api/sections/${sectionId}/pluslets`
+        + (filters ? '?' + new URLSearchParams(filters) : ''));
 
     if (!data.ok) {
         throw new Error(data.status + ' ' + data.statusText);
@@ -34,4 +113,122 @@ async function fetchPluslet(plusletId) {
     }
     
     return data.json();
+}
+
+async function updatePluslet({plusletId, data}) {
+    if (plusletId === undefined) throw new Error('"plusletId" field is required to perform update pluslet request.');
+    if (data === undefined) throw new Error('"data" field is required to perform update pluslet request');
+
+    const req = await fetch(`/api/pluslets/${plusletId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (!req.ok) {
+        throw new Error(req.status + ' ' + req.statusText);
+    }
+
+    return req.json();
+}
+
+async function reorderPlusletWithinColumn(sectionId, column, sourceIndex, destinationIndex) {
+    const {'hydra:member': pluslets} = await fetchPluslets(sectionId, {
+        pcolumn: column
+    });
+    
+    // Move pluslet within the same column
+    const [reorderedItem] = pluslets.splice(sourceIndex, 1);
+    pluslets.splice(destinationIndex, 0, reorderedItem);
+
+    // Perform the updating of the section index asynchronously
+    return Promise.all(pluslets.map(async (pluslet, index) => {
+        if (pluslet.prow !== index) {
+            return fetch(`/api/pluslets/${pluslet.plusletId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prow: index
+                })
+            }).catch(error => {
+                console.error(error);
+                throw new Error(error);
+            });
+        }
+    }));
+}
+
+async function reorderPlusletWithinSection(sectionId, sourceColumn, sourceIndex, destinationColumn, destinationIndex) {
+    const {'hydra:member': sourcePluslets} = await fetchPluslets(sectionId, {
+        pcolumn: sourceColumn
+    });
+
+    // Remove pluslet from source column
+    const [reorderedItem] = sourcePluslets.splice(sourceIndex, 1);
+
+    // Reorder source column pluslets
+    await Promise.all(sourcePluslets.map(async (pluslet, index) => {
+        if (pluslet.prow !== index) {
+            return fetch(`/api/pluslets/${pluslet.plusletId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prow: index
+                })
+            }).catch(error => {
+                console.error(error);
+                throw new Error(error);
+            });
+        }
+    }));
+
+    // Move pluslet to a different column
+    const {'hydra:member': destinationPluslets} = await fetchPluslets(sectionId, {
+        pcolumn: destinationColumn
+    });
+
+    // Add to destination column and reorder destination column pluslets 
+    destinationPluslets.splice(destinationIndex, 0, reorderedItem);
+    return Promise.all(destinationPluslets.map(async (pluslet, index) => {
+        if (pluslet.prow !== index || pluslet.pcolumn !== destinationColumn) {
+            return fetch(`/api/pluslets/${pluslet.plusletId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prow: index,
+                    pcolumn: destinationColumn
+                })
+            }).catch(error => {
+                console.error(error);
+                throw new Error(error);
+            });
+        }
+    }));
+}
+
+async function reorderPluslet({sectionId, sourceColumn, sourceIndex, destinationColumn, destinationIndex}) {
+    if (sectionId === undefined) throw new Error('"sectionId" field is required to perform reorder pluslet request.');
+    if (sourceColumn === undefined) throw new Error('"sourceColumn" field is required to perform reorder pluslet request.');
+    if (sourceIndex === undefined) throw new Error('"sourceIndex" field is required to perform reorder pluslet request.');
+    if (destinationColumn === undefined) throw new Error('"destinationColumn" field is required to perform reorder pluslet request.');
+    if (destinationIndex === undefined) throw new Error('"destinationIndex" field is required to perform reorder pluslet request.');
+
+    const {'hydra:member': sourcePluslets} = await fetchPluslets(sectionId, {
+        pcolumn: sourceColumn
+    });
+
+    // Move pluslet within the same column
+    if (sourceColumn === destinationColumn) {
+        return reorderPlusletWithinColumn(sectionId, sourceColumn, sourceIndex, destinationIndex);
+    } else {
+        return reorderPlusletWithinSection(sectionId, sourceColumn, sourceIndex, destinationColumn, destinationIndex);
+    }
 }
