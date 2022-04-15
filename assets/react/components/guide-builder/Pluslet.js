32b0c5a1436656b8d7cc24357be0732566732fc8
Renel Desir <rxd702@miami.edu>
2022-04-15 09:08:07 -0400

    add ckeditor4-react, implement into pluslet component

--- assets/react/components/guide-builder/Pluslet.js
+++ assets/react/components/guide-builder/Pluslet.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDeletePluslet } from '#api/guide/PlusletAPI';
 import { Draggable } from 'react-beautiful-dnd';
+import { CKEditor } from 'ckeditor4-react';
 
 function Pluslet({ plusletId, plusletRow, sectionId }) {
 
+    const [editable, setEditable] = useState(false);
     const deletePlusletMutation = useDeletePluslet(sectionId);
 
     const deletePluslet = () => {
@@ -15,26 +17,31 @@ function Pluslet({ plusletId, plusletRow, sectionId }) {
         }
     }
 
+    const doubleClicked = () => {
+        setEditable(!editable);
+    }
+    
     return (
         <Draggable type="pluslet" key={plusletId.toString()} draggableId={plusletId.toString()} index={plusletRow}>
             {(provided, snapshot) => {
                 return (
-                    <div className="pluslet" key={plusletId} ref={provided.innerRef} 
+                    <div className="pluslet" key={plusletId} ref={provided.innerRef} onDoubleClick={doubleClicked}
                         {...provided.draggableProps}
                         style={{
                             ...provided.draggableProps.style,
                             backgroundColor: '#d5d5d5',
                             height: '200px',
                         }}>
-                            <div>
-                                <div className="drag-handle" {...provided.dragHandleProps} title="Move pluslet">
-                                    <i className="fas fa-arrows-alt"></i>
-                                </div>
-                                <button onClick={deletePluslet} title="Delete pluslet">
-                                    <i className="fas fa-trash"></i>
-                                </button>
+                        <div>
+                            <div className="drag-handle" {...provided.dragHandleProps} title="Move pluslet">
+                                <i className="fas fa-arrows-alt"></i>
                             </div>
-                            {'Pluslet ' + plusletId}
+                            <button onClick={deletePluslet} title="Delete pluslet">
+                                <i className="fas fa-trash"></i>
+                            </button>
+                        </div>
+                        <span className="visually-hidden">{'Pluslet ' + plusletId}</span>
+                        {editable ? <CKEditor editorUrl="/build/ckeditor/ckeditor.js" initData={<p>Hello from CKEditor 4!</p>}/> : <p>Double click me to edit!</p>}
                     </div>
                 );
             }}
