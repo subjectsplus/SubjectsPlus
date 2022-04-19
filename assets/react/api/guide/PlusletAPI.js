1fd5587e9f4e5d33a86457336e9eeb40fb892fc2
Renel Desir <rxd702@miami.edu>
2022-04-19 14:46:05 -0400

    add update pluslet mutation

--- assets/react/api/guide/PlusletAPI.js
+++ assets/react/api/guide/PlusletAPI.js
@@ -46,6 +46,35 @@ export function useCreatePluslet(sectionId) {
     });
 }
 
+export function useUpdatePluslet(sectionId) {
+    if (sectionId === undefined) throw new Error('"sectionId" field is required to call useUpdatePluslet.');
+
+    const queryClient = useQueryClient();
+    return useMutation(updatePluslet, {
+        onMutate: async updatedPluslet => {
+            await queryClient.cancelQueries(['pluslets', sectionId]);
+            const previousPlusletsData = queryClient.getQueryData(['pluslets', sectionId]);
+
+            const optimisticResult = produce(previousPlusletsData, draftData => {
+                const index = draftData['hydra:member'].findIndex(pluslet => pluslet.id === updatedPluslet.plusletId);
+                draftData['hydra:member'][index] = updatedSection.optimisticResult;
+            });
+            
+            queryClient.setQueryData(['pluslets', sectionId], optimisticResult);
+            return { previousPlusletsData };
+        },
+        onError: (error, plusletData, context) => {
+            // Perform rollback of pluslets mutation
+            console.error(error);
+            queryClient.setQueryData(['pluslets', sectionId], context.previousPlusletsData);
+        },
+        onSettled: () => {
+            // Refetch the pluslets data
+            queryClient.invalidateQueries(['pluslets', sectionId]);
+        },
+    });
+}
+
 export function useDeletePluslet(sectionId) {
     if (sectionId === undefined) throw new Error('"sectionId" field is required to call useDeletePluslet.');
 
