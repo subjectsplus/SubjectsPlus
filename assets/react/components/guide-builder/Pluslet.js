309c87164b3dd02960b757473eb913ec0b9dcbb3
Renel Desir <rxd702@miami.edu>
2022-04-25 11:55:06 -0400

    add control s as save key for current editable pluslet

--- assets/react/components/guide-builder/Pluslet.js
+++ assets/react/components/guide-builder/Pluslet.js
@@ -5,7 +5,8 @@ import { Draggable } from 'react-beautiful-dnd';
 import { useDebouncedCallback } from 'use-debounce';
 import DOMPurify from 'dompurify';
 
-function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, currentEditablePluslet, currentEditablePlusletCallBack }) {
+function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, currentEditablePluslet, 
+    currentEditablePlusletCallBack }) {
 
     const [editable, setEditable] = useState(false);
     const [title, setTitle] = useState(plusletTitle);
@@ -34,7 +35,6 @@ function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, 
     }
 
     const toggleEditable = () => {
-        console.log('currentEditablePluslet: ', currentEditablePluslet);
         if (currentEditablePluslet !== plusletId) {
             setEditable(true);
             currentEditablePlusletCallBack(plusletId);
@@ -43,6 +43,17 @@ function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, 
             currentEditablePlusletCallBack('');
             updatePlusletTitle();
             updatePlusletBody();
+        }
+    }
+
+    const handleSaveKey = (event) => {
+        if (currentEditablePluslet === plusletId) {
+            if ((event.ctrlKey || event.metaKey) && event.key === 's') {
+                event.preventDefault();
+                currentEditablePlusletCallBack('');
+                updatePlusletTitle();
+                updatePlusletBody();
+            }
         }
     }
 
@@ -71,8 +82,10 @@ function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, 
     const debouncedUpdatePlusletBody = useDebouncedCallback(updatePlusletBody, 1000);
 
     const onCKEditorChanged = evt => {
-        setBody(evt.editor.getData())
-        debouncedUpdatePlusletBody();
+        if (evt.editor) {
+            setBody(evt.editor.getData())
+            debouncedUpdatePlusletBody();
+        }
     }
     
     const editableTitle = () => {
@@ -123,7 +136,7 @@ function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, 
 
     const editor = () => {
         if (editable) {
-            return (<CKEditor initData={body} onChange={onCKEditorChanged} />);
+            return (<CKEditor initData={body} onKey={evt => handleSaveKey(evt.data.domEvent.$)} onChange={onCKEditorChanged} />);
         } else {
             return (<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(body)}} />);
         }
@@ -134,6 +147,7 @@ function Pluslet({ plusletId, plusletTitle, plusletBody, plusletRow, sectionId, 
             {(provided, snapshot) => {
                 return (
                     <div className="pluslet" key={plusletId} ref={provided.innerRef} onDoubleClick={toggleEditable}
+                        onKeyDown={handleSaveKey}
                         {...provided.draggableProps}
                         style={{
                             ...provided.draggableProps.style,
