eef1cb9ec936635717219349d896abea71b61799
Renel Desir <rxd702@miami.edu>
2022-04-28 13:04:34 -0400

    update token title and description before render

--- src/Service/TokenService.php
+++ src/Service/TokenService.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Title;
 
 class TokenService {
+    private const DESCRIPTION_BLOCK_CLASS_NAME = 'record-description';
     private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager) {
@@ -32,12 +33,20 @@ class TokenService {
             $recordElement = $titleId->parentNode;
 
             // get up to date record token info
+            /** @var Title $recordInfo */
             $recordInfo = $titleRepo->findOneBy(['titleId' => $titleId->value]);
 
-            // check for link tags
             foreach ($recordElement->childNodes as $childNode) {
+                // TODO: caching and updating on the database
+                // check for link tags
                 if (strtolower($childNode->nodeName) === 'a') {
                     $childNode->setAttribute('href', $recordInfo->getLocation()[0]->getLocation());
+                    $childNode->nodeValue = $recordInfo->getTitle();
+                }
+
+                // check for description blocks
+                if (strtolower($childNode->nodeName) === 'span' && $childNode->getAttribute('class') === self::DESCRIPTION_BLOCK_CLASS_NAME) {
+                    $childNode->nodeValue = $recordInfo->getDescription();
                 }
             }
         }
