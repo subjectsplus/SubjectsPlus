01d90fec550eda3857c96671bcb9dcebc40d034f
Renel Desir <rxd702@miami.edu>
2022-05-02 15:59:49 -0400

    implement fetch media by staff with react query

--- assets/react/api/media/MediaAPI.js
+++ assets/react/api/media/MediaAPI.js
@@ -1,5 +1,20 @@
 
-export function fetchMedia(filters = null) {
+import { useQuery } from 'react-query';
+
+export function useFetchMediaByStaff(staffId) {
+    if (staffId === undefined) throw new Error('"staffId" field is required to call useFetchMediaByStaff.');
+
+    return useQuery(['media', staffId], 
+        () => fetchMedia({
+            staff: '/api/staff/' + staffId
+        }), {
+            select: data => data['hydra:member'],
+            staleTime: 5000,
+        }
+    );
+}
+
+async function fetchMedia(filters = null) {
     const data = await fetch(`/api/media`
     + (filters ? '?' + new URLSearchParams(filters) : ''));
 
