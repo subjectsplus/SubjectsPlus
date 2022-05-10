29b92b3312ad051081cb976d1bde1f2922143e71
Renel Desir <rxd702@miami.edu>
2022-05-10 11:29:10 -0400

    switch places of the alt text and caption text fields

--- assets/react/components/media-browser/MediaUploadForm.js
+++ assets/react/components/media-browser/MediaUploadForm.js
@@ -19,18 +19,6 @@ function MediaUploadForm({ defaultTitle, disabled, onSubmit, onCancel }) {
                         required="required"
                     />
 
-                    {/* Media Caption text field */}
-                    <label htmlFor="media-caption-field" className="form-label">
-                        <span className="visually-hidden">Caption</span>
-                    </label>
-                    <input
-                        type="text"
-                        name="caption"
-                        id="media-caption-field"
-                        placeholder="Caption"
-                        autoComplete="off"
-                    />
-
                     {/* Media AltText text field */}
                     <label htmlFor="media-alt-text-field" className="form-label">
                         <span className="visually-hidden">Alt Text</span>
@@ -40,6 +28,18 @@ function MediaUploadForm({ defaultTitle, disabled, onSubmit, onCancel }) {
                         name="altText"
                         id="media-alt-text-field"
                         placeholder="Alt Text"
+                        autoComplete="off"
+                    />
+
+                    {/* Media Caption text field */}
+                    <label htmlFor="media-caption-field" className="form-label">
+                        <span className="visually-hidden">Caption</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="caption"
+                        id="media-caption-field"
+                        placeholder="Caption"
                         autoComplete="off"
                     />
 
