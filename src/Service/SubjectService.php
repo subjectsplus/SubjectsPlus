b260a5b3915476741872c97ab7566c90b680e050
cbrownroberts <charlesbrownroberts@miami.edu>
2022-05-10 15:01:26 -0400

    rudimentry frontend subject and show subject

---
+++ src/Service/SubjectService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\SubjectRepository;
+
+class SubjectService
+{
+    /**
+     * @var SubjectRepository
+     */
+    private $subjectRepository;
+
+    public function __construct(SubjectRepository $subjectRepository)
+    {
+        $this->subjectRepository = $subjectRepository;
+    }
+
+    /**
+     * @return \App\Entity\Subject[]
+     */
+    public function getSubjectIndex()
+    {
+        return $this->subjectRepository->findAllSubjectsAlphabetical();
+    }
+
+    public function getSubjectByShortForm($shortform)
+    {
+        return $this->subjectRepository->findSubjectByShortForm($shortform);
+    }
+
+}
