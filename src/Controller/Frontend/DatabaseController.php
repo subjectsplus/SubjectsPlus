9359754dc9eb33a96464eb80e9704be550f3995f
cbrownroberts <charlesbrownroberts@miami.edu>
2022-05-11 12:24:33 -0400

    added twig for new and trial db

--- src/Controller/Frontend/DatabaseController.php
+++ src/Controller/Frontend/DatabaseController.php
@@ -5,16 +5,28 @@ namespace App\Controller\Frontend;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\DatabaseService;
 
 class DatabaseController extends AbstractController
 {
+    private $databaseService;
+
+    public function __construct(DatabaseService $databaseService)
+    {
+        $this->databaseService = $databaseService;
+    }
+
     /**
-     * @Route("/frontend/database", name="frontend_database")
+     * @Route("/databases", name="frontend_database")
      */
     public function index(): Response
     {
         return $this->render('frontend/database/index.html.twig', [
             'controller_name' => 'DatabaseController',
+            //'letters' => getLetters('databases'),
+            'newestDatabases' => $this->databaseService->newestDatabases(),
+            'trialDatabases' => $this->databaseService->getTrialDatabases(),
+
         ]);
     }
 }
