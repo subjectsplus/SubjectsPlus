ca6ff6a55e1feaca8d98f60f1ea2ec14c66d4ce4
pvillanueva <p.villanueva@miami.edu>
2022-05-20 16:54:32 -0400

    Styles for guide metadata form inside guide builder

--- assets/react/components/guide-metadata/GuideMetadataForm.js
+++ assets/react/components/guide-metadata/GuideMetadataForm.js
@@ -5,78 +5,106 @@ function GuideMetadataForm({ guide, disabled, onSubmit }) {
         <form action={void(0)} onSubmit={onSubmit}>
                 <fieldset disabled={disabled}>
                     {/* Guide Shortform text field */}
-                    <div id="guide-shortform">
-                        <label htmlFor="guide-shortform-field" className="form-label">
-                            Shortform
-                        </label>
-                        {' '}
-                        <input
-                            type="text"
-                            name="shortform"
-                            id="guide-shortform-field"
-                            placeholder="Shortform"
-                            autoComplete="off"
-                            defaultValue={guide.shortform}
-                            required="required"
-                        />
+                    <div id="guide-shortform" className="mb-2">
+                        <div className="row">
+                            <div className="col-3">
+                                <label htmlFor="guide-shortform-field" className="form-label fs-sm form-required">
+                                    Short Form
+                                </label>
+                                {' '}
+                            </div>
+                            <div className="col-9">
+                                <input
+                                    type="text"
+                                    name="shortform"
+                                    id="guide-shortform-field"
+                                    placeholder="Shortform"
+                                    autoComplete="off"
+                                    defaultValue={guide.shortform}
+                                    required="required"
+                                    className="form-control form-control-sm"
+                                />
+                            </div>
+                        </div>
                     </div>
 
                     {/* Guide Subject text field */}
-                    <div id="guide-subject">
-                        <label htmlFor="guide-subject-field" className="form-label">
-                            Guide Title
-                        </label>
-                        {' '}
-                        <input
-                            type="text"
-                            name="subject"
-                            id="guide-subject-field"
-                            placeholder="Title"
-                            autoComplete="off"
-                            defaultValue={guide.subject}
-                            required="required"
-                        />
+                    <div id="guide-subject" className="mb-2">
+                        <div className="row">
+                            <div className="col-3">
+                                <label htmlFor="guide-subject-field" className="form-label fs-sm form-required">
+                                    Guide Title
+                                </label>
+                                {' '}
+                            </div>
+                            <div className="col-9">
+                                <input
+                                    type="text"
+                                    name="subject"
+                                    id="guide-subject-field"
+                                    placeholder="Title"
+                                    autoComplete="off"
+                                    defaultValue={guide.subject}
+                                    required="required"
+                                    className="form-control form-control-sm"
+                                />
+                            </div>
+                        </div>
                     </div>
                     
                      {/* Guide Type select field */}
-                     <div id="guide-type">
-                        <label htmlFor="guide-type-field" className="form-label">
-                            Type
-                        </label>
-                        {' '}
-                        <select id="guide-type-field" name="type" defaultValue={guide.type}>
-                            <option value="Subject">Subject</option>
-                            <option value="Topic">Topic</option>
-                            <option value="Course">Course</option>
-                            <option value="Placeholder">Placeholder</option>
-                        </select>
+                    {/* TODO: Apply react-select style */}
+                     <div id="guide-type" className="mb-2">
+                         <div className="row">
+                             <div className="col-3">
+                                 <label htmlFor="guide-type-field" className="form-label fs-sm form-required">
+                                     Type
+                                 </label>
+                                 {' '}
+                             </div>
+                             <div className="col-9">
+                                 <select id="guide-type-field" name="type" defaultValue={guide.type} className="form-select form-select-sm">
+                                     <option value="Subject">Subject</option>
+                                     <option value="Topic">Topic</option>
+                                     <option value="Course">Course</option>
+                                     <option value="Placeholder">Placeholder</option>
+                                 </select>
+                             </div>
+                         </div>
                     </div>
 
                     {/* Guide Active select field */}
-                    <div id="guide-active">
-                        <label htmlFor="guide-active-field" className="form-label">
-                            Status
-                        </label>
-                        {' '}
-                        <select id="guide-active-field" name="active" defaultValue={guide.active}>
-                            <option value="0">Inactive</option>
-                            <option value="1">Active</option>
-                            <option value="2">Suppressed</option>
-                        </select>
+                    {/* TODO: Apply react-select style */}
+                    <div id="guide-active" className="mb-2">
+                        <div className="row">
+                            <div className="col-3">
+                                <label htmlFor="guide-active-field" className="form-label fs-sm form-required">
+                                    Status
+                                </label>
+                                {' '}
+                            </div>
+                            <div className="col-9">
+                                <select id="guide-active-field" name="active" defaultValue={guide.active} className="form-select form-select-sm">
+                                    <option value="0">Inactive</option>
+                                    <option value="1">Active</option>
+                                    <option value="2">Suppressed</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
 
                     {/* Guide Description textarea field */}
-                    <div id="guide-description">
-                        <label htmlFor="guide-description-field" className="form-label">
+                    <div id="guide-description" className="mb-3">
+                        <label htmlFor="guide-description-field" className="form-label fs-sm">
                             Description
                         </label>
                         {' '}
-                        <textarea id="guide-description-field" name="description" cols="30" rows="8" defaultValue={guide.description} />
+                        <textarea id="guide-description-field" name="description" cols="30" rows="6" defaultValue={guide.description} className="form-control form-control-sm" />
                     </div>
 
                     {/* Submit Button */}
                     <div id="save-guide-metadata">
-                        <input type="submit" value="Save Guide Details" />
+                        <input type="submit" value="Save Guide Details" className="btn btn-gradient btn-round btn-sm" />
                     </div>
                 </fieldset>
         </form>
