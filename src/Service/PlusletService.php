94291b8d722a1abc14fb85ec85d1086dd1c33b85
Renel Desir <rxd702@miami.edu>
2022-06-17 08:36:59 -0400

    implement clone pluslet type

--- src/Service/PlusletService.php
+++ src/Service/PlusletService.php
@@ -1,9 +1,17 @@
 <?php
 
 namespace App\Service;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Pluslet;
 
 class PlusletService
 {
+    private $plusletRepository;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->plusletRepository = $entityManager->getRepository(Pluslet::class);
+    }
+
     public function plusletClassName(string $type): string
     {
         if ($type == "Pluslet") {
@@ -13,5 +21,11 @@ class PlusletService
         } else {
             return '\\SubjectsPlus\\Control\\Pluslet\\Pluslet_'.$type;
         }
+    }
+
+    public function getPluslet(int $plusletId) {
+        return $this->plusletRepository->findOneBy([
+            'plusletId' => $plusletId
+        ]);
     }
 }
