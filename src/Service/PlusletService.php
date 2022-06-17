f4d938615f6bf07af0dc03aa1092739f1bb582dd
Renel Desir <rxd702@miami.edu>
2022-06-17 08:40:12 -0400

    toPublicArray and plusletClassName not needed due to twig subject render

--- src/Service/PlusletService.php
+++ src/Service/PlusletService.php
@@ -12,17 +12,6 @@ class PlusletService
         $this->plusletRepository = $entityManager->getRepository(Pluslet::class);
     }
 
-    public function plusletClassName(string $type): string
-    {
-        if ($type == "Pluslet") {
-            return '\\SubjectsPlus\\Control\\Pluslet';
-        } elseif ($type == "PlusletInterface") {
-            return '\\SubjectsPlus\\Control\\Pluslet\\PlusletInterface';
-        } else {
-            return '\\SubjectsPlus\\Control\\Pluslet\\Pluslet_'.$type;
-        }
-    }
-
     public function getPluslet(int $plusletId) {
         return $this->plusletRepository->findOneBy([
             'plusletId' => $plusletId
