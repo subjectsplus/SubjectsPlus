a72cadd69442cce05fd64111319d1aa366a4a71b
Renel Desir <rxd702@miami.edu>
2022-06-22 09:18:20 -0400

    add description type data attribute

--- src/CKEditorConfig/plugins/recordtoken/plugin.js
+++ src/CKEditorConfig/plugins/recordtoken/plugin.js
@@ -9,17 +9,17 @@
     const linkTemplate = '<a class="' + linkClass + '" href="{recordLink}">{recordTitle}</a>',
         descriptionTemplate = '<span class="' + descriptionClass + '">{recordDescription}</span>',
         iconTemplate = '<button class="' + iconClass + '" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="{recordDescription}"><img src="' + iconSource +'" /></button>',
-        template = '<span class="' + tokenClass + '" data-record-id="{recordId}">' +
+        template = '<span class="' + tokenClass + '" data-record-id="{recordId}" data-description-type="none">' +
                     linkTemplate +
                     '</span>&nbsp;';
     
     const templateBlock = new CKEDITOR.template(function(data) {
         if (data.descriptionType == 'block') {
-            return '<span class="' + tokenClass + '" data-record-id="{recordId}">' +
+            return '<span class="' + tokenClass + '" data-record-id="{recordId}" data-description-type="block">' +
                 linkTemplate + descriptionTemplate +
                     '</span>&nbsp;';
         } else if (data.descriptionType == 'icon') {
-            return '<span class="' + tokenClass + '" data-record-id="{recordId}">' +
+            return '<span class="' + tokenClass + '" data-record-id="{recordId}" data-description-type="icon">' +
             linkTemplate + iconTemplate +
                 '</span>&nbsp;';
         } else {
@@ -69,7 +69,7 @@
 
                     // Set record data, call api and index if unavailable
                     if (window.records[recordId]) {
-                        console.log('Record for id', recordId, ' already indexed, skipping retrieval.');
+                        console.log('Record for id', recordId, 'already indexed, skipping retrieval.');
                         record = window.records[recordId];
                         this.setData('record', record);
                     } else {
@@ -185,6 +185,7 @@
                         // Description is null or empty
                         // Set description type to none
                         this.setData('descriptionType', 'none');
+                        this.element.data('description-type', 'none');
                         return;
                     }
 
@@ -215,6 +216,7 @@
                         this.element.appendHtml(html);
                     }
 
+                    this.element.data('description-type', newDescriptionType);
                     this.oldDescriptionType = newDescriptionType;
 
                     editor.fire('saveSnapshot');
