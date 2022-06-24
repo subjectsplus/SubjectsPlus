da1c3edeed48cff17489731b45e8c961070a79d1
Renel Desir <rxd702@miami.edu>
2022-06-24 09:48:21 -0400

    create service for link list migration helper methods

--- migrations/Version20220621134441.php
+++ migrations/Version20220621134441.php
@@ -6,26 +6,16 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Masterminds\HTML5;
-use App\Service\UtilityService;
+use App\Service\Migration\LinkListMigrationService;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220621134441 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Migrate LinkList pluslet.';
+        return 'Migrate LinkList pluslet of type "LinkList".';
     }
 
-    private function isLinkListDisplay(string $html): bool {
-        return str_contains($html, '<ul class="link-list-display">') ||
-            str_contains($html, "<ul class='link-list-display'>") ||
-            str_contains($html, '<li>{{dab}');
-    }
-
-    private function getRecord(string $recordId) {
+    public function getRecord(string $recordId) {
         return $this->connection->fetchAssociative(
             'SELECT title.title_id AS title_id, title, description, location FROM title, location
             WHERE title.title_id = :title_id AND location.title_id = title.title_id AND format = 1', 
@@ -33,83 +23,6 @@ final class Version20220621134441 extends AbstractMigration
                 ':title_id' => $recordId
             ]
         );
-    }
-
-    private function getUpdatedLinkListDisplay(string $html) {
-        // Load the html for parsing
-        $html5 = new HTML5([
-            'disable_html_ns' => true,
-        ]);
-
-        $doc = $html5->loadHTML($html);
-        $xpath = new \DOMXPath($doc);
-
-        // Query for all li tags
-        $oldListItems = $xpath->query('//li');
-
-        foreach ($oldListItems as $oldListItem) {
-            // Token configuration with format {{dab},{title_id},{title},{extraConfig}}
-            $tokenString = trim($oldListItem->nodeValue, '{}');
-            $tokenConfig = preg_split('/},\s?{/', $tokenString);
-            $recordId = isset($tokenConfig[1]) ? $tokenConfig[1] : null;
-            
-            if ($recordId) {
-                $extraConfig = str_split($tokenConfig[3]);
-                $showIcon = isset($extraConfig[0]) ? $extraConfig[0] : false;
-                $showDescription = isset($extraConfig[1]) ? $extraConfig[1] : false;
-                $showNotes = isset($extraConfig[2]) ? $extraConfig[2] : false;
-
-                // Get most up to date record data
-                $recordData = $this->getRecord($recordId);
-
-                if ($recordData) {
-                    // Create the token span element
-                    $tokenElement = $doc->createElement('span');
-                    $tokenElement->setAttribute('class', 'record-token');
-                    $tokenElement->setAttribute('data-record-id', $recordId);
-                    $tokenElement->setAttribute('data-description-type', 'none');
-
-                    if ($showIcon) $tokenElement->setAttribute('data-show-icon', 'true');
-                    if ($showNotes) $tokenElement->setAttribute('data-show-notes', 'true');
-
-                    // Create and append the token link element
-                    $recordLink = $doc->createElement('a');
-                    $recordLink->nodeValue = UtilityService::cleanString($recordData['title']);
-                    $recordLink->setAttribute('class', 'record-link');
-                    $recordLink->setAttribute('href', $recordData['location']);
-                    $tokenElement->appendChild($recordLink);
-                    
-                    // Create and append the token description element if applicable
-                    if ($showDescription) {
-                        $tokenElement->setAttribute('data-description-type', 'block');
-                        
-                        $descriptionElement = $doc->createElement('span');
-                        $descriptionElement->nodeValue = UtilityService::cleanString($recordData['description']);
-                        $descriptionElement->setAttribute('class', 'record-description');
-                        
-                        $brElement = $doc->createElement('br');
-                        
-                        $tokenElement->appendChild($brElement);
-                        $tokenElement->appendChild($descriptionElement);
-                    }
-
-                    // Create a list item element and append token span element
-                    $newListItem = $doc->createElement('li');
-                    $newListItem->appendChild($tokenElement);
-
-                    // Replace old list item element with new list item element
-                    $oldListItem->parentNode->replaceChild($newListItem, $oldListItem);
-                } else {
-                    // Remove the nonexistant record
-                    $oldListItem->parentNode->removeChild($oldListItem);
-                }
-            } else {
-                // Remove the nonexistant record
-                $oldListItem->parentNode->removeChild($oldListItem);
-            }
-        }
-
-        return $doc->saveHTML();
     }
 
     public function up(Schema $schema): void
@@ -122,8 +35,8 @@ final class Version20220621134441 extends AbstractMigration
             $body = $linkList['body'];
 
             if ($body) {
-                if ($this->isLinkListDisplay($body)) {
-                    $newBody = $this->getUpdatedLinkListDisplay($body);
+                if (LinkListMigrationService::isLinkListDisplay($body)) {
+                    $newBody = LinkListMigrationService::getUpdatedLinkListDisplay($body, [$this, 'getRecord']);
 
                     if ($newBody) {
                         $this->addSql('UPDATE pluslet SET body = :body, type = "Basic" WHERE pluslet_id = :pluslet_id', [
