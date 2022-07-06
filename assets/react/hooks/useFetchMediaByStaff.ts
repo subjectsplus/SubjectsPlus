6a8671023277f0e87fea6e78a6f4b1254a051ee2
Renel Desir <rxd702@miami.edu>
2022-07-06 09:37:22 -0400

    remove redundant undefined check

--- assets/react/hooks/useFetchMediaByStaff.ts
+++ assets/react/hooks/useFetchMediaByStaff.ts
@@ -3,8 +3,6 @@ import { MediaType } from '@shared/types/media_types';
 import { fetchMedia } from '@api/media/MediaAPI';
 
 export const useFetchMediaByStaff = (staffId: number) => {
-    if (staffId === undefined) throw new Error('"staffId" field is required to call useFetchMediaByStaff.');
-
     return useQuery<Record<string, any>, Error>(['media', staffId], 
         () => fetchMedia({
             staff: staffId
