6066c27b30e1ed06cd2faecc6023658498ca2e12
Renel Desir <rxd702@miami.edu>
2022-07-07 16:30:01 -0400

    use guidesectiontype as return type

--- assets/react/hooks/useFetchSection.ts
+++ assets/react/hooks/useFetchSection.ts
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { fetchSection } from '@api/guide/SectionAPI';
+import { GuideSectionType } from '@shared/types/guide_types';
 
 export const useFetchSection = (sectionUUID: string) => {
-    return useQuery<Record<string, any>, Error>(['section', sectionUUID], () => fetchSection(sectionUUID));
+    return useQuery<GuideSectionType, Error>(['section', sectionUUID], () => fetchSection(sectionUUID));
 }
