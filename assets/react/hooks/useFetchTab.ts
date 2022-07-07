6d4f53e1f51ba636d40a63b99f134d610260aee1
Renel Desir <rxd702@miami.edu>
2022-07-07 16:29:48 -0400

    use guidetabtype as return type

--- assets/react/hooks/useFetchTab.ts
+++ assets/react/hooks/useFetchTab.ts
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { fetchTab } from '@api/guide/TabAPI';
+import { GuideTabType } from '@shared/types/guide_types';
 
 export const useFetchTab = (tabUUID: string, enabled: boolean = true) => {
-    return useQuery<Record<string, any>, Error>(['tab', tabUUID], () => fetchTab(tabUUID), { enabled: enabled });
+    return useQuery<GuideTabType, Error>(['tab', tabUUID], () => fetchTab(tabUUID), { enabled: enabled });
 }
