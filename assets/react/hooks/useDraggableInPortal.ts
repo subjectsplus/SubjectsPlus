55ca3223d385185cda17bfe1e00d935f790b65c6
Renel Desir <rxd702@miami.edu>
2022-07-11 14:24:56 -0400

    refactor hook into typescript

--- assets/react/hooks/useDraggableInPortal.ts
+++ assets/react/hooks/useDraggableInPortal.ts
@@ -1,8 +1,9 @@
+import { DraggableProvided, DraggableStateSnapshot, DraggingStyle } from 'react-beautiful-dnd'
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export const useDraggableInPortal = () => {
-    const element = useRef(document.createElement('div')).current;
+    const element = useRef<HTMLDivElement>(document.createElement('div')).current;
 
     useEffect(() => {
         if (element) {
@@ -20,9 +21,11 @@ export const useDraggableInPortal = () => {
         }
       }, [element]);
 
-    return (render) => (provided, ...args) => {
-        const result = render(provided, ...args);
-        const style = provided.draggableProps.style
+      
+    return (render: (provided: DraggableProvided, snapshot: DraggableStateSnapshot) => React.ReactElement) => 
+      (provided: DraggableProvided, snapshot: DraggableStateSnapshot) => {
+        const result = render(provided, snapshot);
+        const style = provided.draggableProps.style as DraggingStyle;
         if (style.position === 'fixed') {
             return createPortal(result, element);
         }
