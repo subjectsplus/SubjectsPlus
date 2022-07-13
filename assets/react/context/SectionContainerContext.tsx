b19e1c505fa039a0d89f97d614b40b80e5addec5
Renel Desir <rxd702@miami.edu>
2022-07-13 13:56:50 -0400

    remove unnecessary import

--- assets/react/context/SectionContainerContext.tsx
+++ assets/react/context/SectionContainerContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from 'react';
-import { GuideTabType } from '@shared/types/guide_types';
 
 type SectionContainerProviderProps = {
   currentDraggingId: string|null,
