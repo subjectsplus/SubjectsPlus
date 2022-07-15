f99386d60e3ae7be5ffa2aab2aeaa86e4f7e4f89
Renel Desir <rxd702@miami.edu>
2022-07-15 11:25:06 -0400

    when a save is requested, trigger the submit event

--- assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/EditSubjectSpecialist.tsx
+++ assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/EditSubjectSpecialist.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from 'react';
 import { useGuideTabContainer, GuideTabContainerType } from '@context/GuideTabContainerContext';
+import { usePlusletWindow, PlusletWindowType } from '@context/PlusletWindowContext';
 import { useFetchSubjectSpecialists } from '@hooks/useFetchSubjectSpecialists';
 import { SubjectSpecialistForm } from './SubjectSpecialistForm';
 
@@ -8,7 +10,23 @@ type EditSubjectSpecialistProps = {
 
 export const EditSubjectSpecialist = ({ extra }: EditSubjectSpecialistProps) => {
     const { subjectId } = useGuideTabContainer() as GuideTabContainerType;
+    const { savePlusletCallback, isSaveRequested } = usePlusletWindow() as PlusletWindowType;
     const { isLoading, isError, data, error } = useFetchSubjectSpecialists(subjectId);
+
+    const formRef = useRef<HTMLFormElement>(null);
+
+    useEffect(() => {
+        if (isSaveRequested) {
+            console.log('formRef.current:', formRef.current);
+            formRef.current?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+        }
+    }, [isSaveRequested]);
+
+    const handleUpdate = (data: Record<string, Record<string, boolean>>) => {
+        console.log('data:', data);
+        // formulate the new extra field
+        const newExtra = {};
+    }
 
     if (isLoading) {
         return (<p>Loading Subject Specialists...</p>);
@@ -17,9 +35,9 @@ export const EditSubjectSpecialist = ({ extra }: EditSubjectSpecialistProps) => 
         return (<p>Error: Failed to load Subject Specialists through API Endpoint!</p>);
     } else if (data) {
         return (
-            <SubjectSpecialistForm specialists={data} extra={extra} />
+            <SubjectSpecialistForm specialists={data} extra={extra} formRef={formRef} onSubmit={handleUpdate}/>
         );
     } else {
-        return (<p>Error: No tabs exist for this guide!</p>);
+        return (<p>Error: No Subject Specialists exist for this guide!</p>);
     }
 }
