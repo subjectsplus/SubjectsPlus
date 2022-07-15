7b469dd083ad4b44838eb7ebd19426e77ff741b7
Renel Desir <rxd702@miami.edu>
2022-07-15 11:24:32 -0400

    use react form hook

--- assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/SubjectSpecialistForm.tsx
+++ assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/SubjectSpecialistForm.tsx
@@ -1,16 +1,21 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 import Form from 'react-bootstrap/Form';
 import { StaffType } from '@shared/types/staff_types';
 
 type SubjectSpecialistFormProps = {
     specialists: StaffType[],
     extra?: Record<string, any>|null,
+    formRef: React.RefObject<HTMLFormElement>,
+    onSubmit: SubmitHandler<any>
 };
 
-const flags = ['showName', 'showTitle', 'showEmail', 'showPhone'];
+const flags = ['showPhoto', 'showName', 'showTitle', 'showEmail', 'showPhone'];
 
-export const SubjectSpecialistForm = ({ specialists, extra }: SubjectSpecialistFormProps) => {
+export const SubjectSpecialistForm = ({ specialists, extra, formRef, onSubmit }: SubjectSpecialistFormProps) => {
+    const { register, handleSubmit } = useForm();
+
     return (
-        <Form noValidate={true} id="specialist-form">
+        <Form noValidate={true} ref={formRef} onSubmit={handleSubmit(onSubmit)} id="specialist-form">
            {specialists.map(specialist => (
                 <Form.Group key={'specialist-info-edit-' + specialist.staffId} className="mb-3" controlId={'form-specialist-' + specialist.staffId}>
                     <h4>{specialist.fname + ' ' + specialist.lname}</h4>
@@ -24,7 +29,8 @@ export const SubjectSpecialistForm = ({ specialists, extra }: SubjectSpecialistF
                             <Form.Check type="switch"
                                 key={'specialist-' + specialist.staffId + '-' + flag + '-switch'}
                                 id={'specialist-' + specialist.staffId + '-' + flag + '-switch'}
-                                checked={flagValue} label={flag} />
+                                defaultChecked={flagValue} label={flag} 
+                                {...register('extra[specialist-' + specialist.staffId.toString() + '][' + flag + ']')} />
                         );
                     })}
                 </Form.Group>
