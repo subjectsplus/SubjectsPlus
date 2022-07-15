def24185883b813a2bb77d0f2da58749df99d034
Charles <cgb37@miami.edu>
2022-07-15 13:11:02 -0400

    composer install hslavich_onelogin_saml

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Hslavich\OneloginSamlBundle\HslavichOneloginSamlBundle::class => ['all' => true],
 ];
