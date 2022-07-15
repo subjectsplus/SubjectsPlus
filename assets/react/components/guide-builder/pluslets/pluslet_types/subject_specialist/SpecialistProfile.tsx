a24d1774c14d440bced0e62078c86f47ff237a54
Renel Desir <rxd702@miami.edu>
2022-07-15 11:25:19 -0400

    use html entity decode for staff title

--- assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/SpecialistProfile.tsx
+++ assets/react/components/guide-builder/pluslets/pluslet_types/subject_specialist/SpecialistProfile.tsx
@@ -1,4 +1,5 @@
 import { StaffType } from '@shared/types/staff_types';
+import { htmlEntityDecode } from '@utility/Utility';
 
 type SpecialistProfileProps = {
     staff: StaffType,
@@ -10,7 +11,7 @@ export const SpecialistProfile = ({staff, flags}: SpecialistProfileProps) => {
         <div className="specialists-info">
             {flags['showName'] && <h4>{staff.fname + ' ' + staff.lname}</h4>}
             <ul className="staff-details">
-                {(flags['showTitle'] && staff.title) && <li>{ staff.title }</li>}
+                {(flags['showTitle'] && staff.title) && <li>{ htmlEntityDecode(staff.title) }</li>}
                 {(flags['showEmail'] && staff.email) && <li>{ staff.email }</li>}
                 {(flags['showPhone'] && staff.tel) && <li>{ staff.tel }</li>}
             </ul>
