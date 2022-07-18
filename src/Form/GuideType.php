d859204cf477308d2fd542966c6c65d91ea21ab1
Renel Desir <rxd702@miami.edu>
2022-07-18 10:03:34 -0400

    add staff field to guide form

--- src/Form/GuideType.php
+++ src/Form/GuideType.php
@@ -3,15 +3,28 @@
 namespace App\Form;
 
 use App\Entity\Subject;
+use App\Entity\Staff;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\StaffRepository;
 
 class GuideType extends AbstractType
 {
+    /**
+     * @var staffRepository;
+     */
+    private $staffRepository;
+
+    public function __construct(StaffRepository $staffRepository)
+    {
+        $this->staffRepository = $staffRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -41,8 +54,20 @@ class GuideType extends AbstractType
             ])
             ->add('description', TextareaType::class, [
                 'required' => false,
+            ])
+            ->add('staff', EntityType::class, [
+                'choices' => $this->staffRepository->findBy(['active' => true]),
+                'class' => Staff::class,
+                'required' => true,
+                'mapped' => false,
+                'choice_label' => function(Staff $staff) {
+                    return $staff->getFname() . ' ' . $staff->getLname();
+                },
+                'multiple' => true,
+                'placeholder' => 'Search and add',
+                'attr' => ['data-placeholder-text' => 'Search and add'],
+                'expanded' => false,
             ]);
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
