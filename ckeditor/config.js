2e031ed81a3f6f1338860ad47d49d311f37d3e44
Charles <cgb37@miami.edu>
2022-10-07 12:59:43 -0400

    ckeditor colorbutton with dependencies

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -9,7 +9,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	//added by dgonzalez
-	config.extraPlugins = 'subsplus_asset,subsplus_cat_link,subsplus_resource,subsplus_faq,maximize,justify';
+	config.extraPlugins = 'subsplus_asset,subsplus_cat_link,subsplus_resource,subsplus_faq,maximize,justify,panelbutton,panel,floatpanel,button,colorbutton';
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
@@ -41,7 +41,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.toolbar_SubsPlus_Narrow =
 		[
 
-			['Bold','Italic','JustifyLeft','JustifyCenter','JustifyRight','-','BulletedList','NumberedList', 'Table', '-','Link','Unlink','Anchor','-','Image','-', 'PasteText','PasteFromWord','-', 'HorizontalRule','-','RemoveFormat', '-','Styles','-','Source'],
+			['Bold','Italic','JustifyLeft','JustifyCenter','JustifyRight','-','BulletedList','NumberedList', 'Table', '-','Link','Unlink','Anchor','-','Image','-', 'PasteText','PasteFromWord','-', 'HorizontalRule','-','RemoveFormat', '-','Styles','-','Source','TextColor','BGColor'],
 
 			'/',
 
@@ -75,6 +75,14 @@ CKEDITOR.editorConfig = function( config ) {
 			['Bold','Italic','-','BulletedList','NumberedList','-','Link','Unlink','-', 'PasteText','PasteFromWord','-', 'Source', 'Maximize']
 
 		];
+	config.colorButton_enableAutomatic = true;
+
+	config.colorButton_colors =
+		'000,800000,8B4513,2F4F4F,008080,000080,4B0082,696969,' +
+		'B22222,A52A2A,DAA520,006400,40E0D0,0000CD,800080,808080,' +
+		'F00,FF8C00,FFD700,008000,0FF,00F,EE82EE,A9A9A9,' +
+		'FFA07A,FFA500,FFFF00,00FF00,AFEEEE,ADD8E6,DDA0DD,D3D3D3,' +
+		'FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';
 
 	config.enterMode = CKEDITOR.ENTER_BR;
 	config.shiftEnterMode = CKEDITOR.ENTER_BR;
