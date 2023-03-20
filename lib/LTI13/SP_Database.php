2e4275aca47440b1d4247ec23dc66779c9d79fcf
Charles <cgb37@miami.edu>
2023-03-20 14:50:29 -0400

    WIP: set $iss to session['iss']

--- lib/LTI13/SP_Database.php
+++ lib/LTI13/SP_Database.php
@@ -9,6 +9,7 @@ $reg_configs = array_diff(scandir(__DIR__ . '/configs'), array('..', '.', '.DS_S
 foreach ($reg_configs as $key => $reg_config) {
     $_SESSION['iss'] = array_merge($_SESSION['iss'], json_decode(file_get_contents(__DIR__ . "/configs/$reg_config"), true));
 }
+$iss = $_SESSION['iss'];
 
 class SP_Database implements LTI\Database {
     public function find_registration_by_issuer($iss)
