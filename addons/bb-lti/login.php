fc6cbb05c51a96dac212c42f53ba03ff51f7f360
Charles <cgb37@miami.edu>
2023-03-20 16:33:39 -0400

    WIP: change redirect url to redirect.php

--- addons/bb-lti/login.php
+++ addons/bb-lti/login.php
@@ -6,6 +6,6 @@ use \IMSGlobal\LTI;
 
 // TODO: this is where we should get the request vars and redirect to lti13.php
 LTI\LTI_OIDC_Login::new(new SP_Database())
-    ->do_oidc_login_redirect(TOOL_HOST . "/addons/bb-lti/lti13.php")
+    ->do_oidc_login_redirect(TOOL_HOST . "/addons/bb-lti/redirect.php")
     ->do_redirect();
 
