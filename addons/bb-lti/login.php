4321b76eb6282a8c52e95e53931f25850f34672a
Charles <cgb37@miami.edu>
2023-03-24 13:21:25 -0400

    comment out var dump

--- addons/bb-lti/login.php
+++ addons/bb-lti/login.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../lib/LTI13/SP_Database.php';
 
 use \IMSGlobal\LTI;
-var_dump($_REQUEST);
+//var_dump($_REQUEST);
 // TODO: this is where we should get the request vars and redirect to lti13.php
 LTI\LTI_OIDC_Login::new(new SP_Database())
     ->do_oidc_login_redirect(TOOL_HOST . "/addons/bb-lti/redirect.php")
