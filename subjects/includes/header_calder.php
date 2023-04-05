e19c2a0fe743c2a021f795ec4ad73e1b99a6e73d
pvillanueva <p.villanueva@miami.edu>
2023-04-05 14:58:51 -0400

    Compile CSS for Calder Theme

--- subjects/includes/header_calder.php
+++ subjects/includes/header_calder.php
@@ -13,7 +13,6 @@
 
     <!-- SP CSS -->
 	<link rel="stylesheet" href="<?php print $AssetPath; ?>css/public/sp-calder.css?v=02082023-1" type="text/css">
-	<link rel="stylesheet" href="<?php print $AssetPath; ?>css/public/sp-calder-temp.css" type="text/css">
 
     <!-- Google Analytics -->
     <?php
